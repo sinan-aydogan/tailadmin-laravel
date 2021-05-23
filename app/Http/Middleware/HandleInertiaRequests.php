f687ef9f3917d43595899445c9808292a840aafb
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 12:34:44 +0300

    Flash message's shared data fix

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,10 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            //Flash Messages
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
+            ],
         ]);
     }
 }
