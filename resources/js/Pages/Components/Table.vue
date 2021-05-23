c752c295a757494824fa28bebbe2b2031a5f778b
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 21:33:27 +0300

    Table sample data added: Users

---
+++ resources/js/Pages/Components/Table.vue
@@ -0,0 +1,70 @@
+<template>
+    <app-layout>
+        <!--Header-->
+        <template #header>
+            Tables
+        </template>
+        <!--Subheader-->
+        <template #subHeader>
+            Customizable tables
+        </template>
+        <!--Content-->
+        <template #default>
+            <div class="my-2 border rounded-md overflow-hidden">
+                <ul class="flex justify-center">
+                    <li class="bg-red-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='red'">
+                        <div v-if="tableColor=='red'" :class="selectorInnerStyle">Red</div>
+                    </li>
+                    <li class="bg-blue-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='blue'">
+                        <div v-if="tableColor=='blue'" :class="selectorInnerStyle">Blue</div>
+                    </li>
+                    <li class="bg-indigo-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='indigo'">
+                        <div v-if="tableColor=='indigo'" :class="selectorInnerStyle">Indigo</div>
+                    </li>
+                    <li class="bg-yellow-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='yellow'">
+                        <div v-if="tableColor=='yellow'" :class="selectorInnerStyle">Yellow</div>
+                    </li>
+                    <li :class="{'bg-gray-600' : tableColor === 'black'}" class="bg-black p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='black'">
+                        <div v-if="tableColor=='black'" :class="selectorInnerStyle">Black</div>
+                    </li>
+                    <li class="bg-gray-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='gray'">
+                        <div v-if="tableColor=='gray'" :class="selectorInnerStyle">Gray</div>
+                    </li>
+                </ul>
+            </div>
+            <t-table :color="tableColor" :content="users" :header="header">
+                <template #photo="{props}">
+                    <div class="flex justify-center">
+                        <t-avatar :link="props.photo" :radius="8" :size="3"/>
+                    </div>
+                </template>
+            </t-table>
+        </template>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import TTable from "@/Components/Table/TTable";
+import TAvatar from "@/Components/Avatar/TAvatar";
+export default {
+    name: "Table",
+    components: {AppLayout,TTable,TAvatar},
+    props: ['users'],
+    data(){
+        return {
+            tableColor : 'blue',
+            header : ['name','email']
+        }
+    },
+    computed:{
+        selectorInnerStyle(){
+            return 'flex-shrink-0 w-full text-center bg-opacity-50 px-2 py-1 bg-white rounded-full font-semibold z-10'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,52 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->prefix('admin')->group(function (){
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+    //Example Pages
+    Route::get('lock',function (){return Inertia::render('Example/Lock');})->name('lock');
+    Route::get('profile',function (){return Inertia::render('Example/Profile');})->name('profile');
+    Route::get('pricing',function (){return Inertia::render('Example/Pricing');})->name('pricing');
+    Route::get('project-management-app',function (){return Inertia::render('Example/ProjectApp');})->name('project-management-app');
+    Route::get('todo-app',function (){return Inertia::render('Example/TodoApp');})->name('todo-app');
+    Route::get('email-app',function (){return Inertia::render('Example/EmailApp');})->name('email-app');
+    Route::get('chat-app',function (){return Inertia::render('Example/ChatApp');})->name('chat-app');
+    //Component Pages
+    Route::get('avatar',function (){return Inertia::render('Components/Avatar');})->name('avatar');
+    Route::get('alert',function (){return Inertia::render('Components/Alert');})->name('alert');
+    Route::get('badge',function (){return Inertia::render('Components/Badge');})->name('badge');
+    Route::get('breadcrumb',function (){return Inertia::render('Components/Breadcrumb');})->name('breadcrumb');
+    Route::get('button',function (){return Inertia::render('Components/Button');})->name('button');
+    Route::get('collapsible',function (){return Inertia::render('Components/Collapsible');})->name('collapsible');
+    Route::get('tab',function (){return Inertia::render('Components/Tab');})->name('tab');
+    Route::get('dropdown',function (){return Inertia::render('Components/Dropdown');})->name('dropdown');
+    Route::get('list',function (){return Inertia::render('Components/List');})->name('list');
+    Route::get('modal',function (){return Inertia::render('Components/Modal');})->name('modal');
+    Route::get('pagination',function (){return Inertia::render('Components/Paginate');})->name('pagination');
+    Route::get('popover',function (){return Inertia::render('Components/Popover');})->name('popover');
+    Route::get('progress',function (){return Inertia::render('Components/Progress');})->name('progress');
+    Route::get('tooltip',function (){return Inertia::render('Components/Tooltip');})->name('tooltip');
+    Route::get('table',function (){return Inertia::render('Components/Table',[
+        'users' => \App\Models\User::all()
+    ]);})->name('table');
+    Route::get('chart',function (){return Inertia::render('Components/Chart');})->name('chart');
+    // Layout Pages
+    Route::get('layout-structure',function (){return Inertia::render('Layouts/LayoutStructure');})->name('layout-structure');
+    Route::get('layout-grid',function (){return Inertia::render('Layouts/Grid');})->name('layout-grid');
+    Route::get('layout-content-box',function (){return Inertia::render('Layouts/ContentBox');})->name('layout-content-box');
+    Route::get('layout-statistic-widget',function (){return Inertia::render('Layouts/StatisticWidget');})->name('layout-statistic-widget');
+    // Form Pages
+    Route::get('form-structure',function (){return Inertia::render('FormElements/FormStructure');})->name('form-structure');
+    Route::get('form-input-group',function (){return Inertia::render('FormElements/InputGroup');})->name('form-input-group');
+    Route::get('form-simple-field',function (){return Inertia::render('FormElements/SimpleField');})->name('form-simple-field');
+    Route::get('form-repeatable-field',function (){return Inertia::render('FormElements/RepeatableField');})->name('form-repeatable-field');
+    Route::get('form-date-field',function (){return Inertia::render('FormElements/DateField');})->name('form-date-field');
+    Route::get('form-select-input',function (){return Inertia::render('FormElements/SelectInput');})->name('form-select-input');
+    Route::get('form-tag-input',function (){return Inertia::render('FormElements/TagInput');})->name('form-tag-input');
+    Route::get('form-validation',function (){return Inertia::render('FormElements/Validation');})->name('form-validation');
+});
+
+
+
