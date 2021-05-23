60e8f94bfd298ef0cdf3cc974bcacb7b42dabe1e
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 12:41:29 +0300

    Tab component added

---
+++ resources/js/Components/Tab/TTabItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div @click="setActiveID">
+            {{title}} {{activeID}}
+        </div>
+        <div v-show="isActive" v-if="tabID === activeID">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import {getCurrentInstance} from "vue";
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Tab'
+    }
+  },
+    data() {
+    return {
+      isActive: true,
+        tabID: getCurrentInstance().uid,
+    }
+  },
+    methods: {
+      setActiveID(){
+          this.$parent.selectedTab(this.tabID)
+      }
+    }
+}
+</script>
