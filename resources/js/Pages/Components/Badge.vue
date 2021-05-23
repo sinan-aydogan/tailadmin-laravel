793dd20929fb17157b8544e368293767bf4209a7
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 16:53:24 +0300

    Badge component added

---
+++ resources/js/Pages/Components/Badge.vue
@@ -0,0 +1,86 @@
+<template>
+    <app-layout>
+        <template #header>
+            Badges
+        </template>
+        <template #subHeader>
+            Useful badges like tags
+        </template>
+        <template #default>
+            <grid-section col="2" gap="4">
+                <!--With Radius-->
+                <t-content-card width="2">
+                    <template #title>
+                        With Radius Options
+                    </template>
+                    <template #content>
+                        <div class="flex flex-wrap gap-4">
+                            <t-badge :radius="0">Radius - 0</t-badge>
+                            <t-badge :radius="1">Radius - 1</t-badge>
+                            <t-badge :radius="2">Radius - 2</t-badge>
+                            <t-badge :radius="3">Radius - 3</t-badge>
+                            <t-badge :radius="4">Radius - 4</t-badge>
+                            <t-badge :radius="5">Radius - 5</t-badge>
+                            <t-badge :radius="6">Radius - 6</t-badge>
+                            <t-badge :radius="7">Radius - 7</t-badge>
+                            <t-badge :radius="8">Radius - 8</t-badge>
+                        </div>
+                    </template>
+                </t-content-card>
+                <!--With Color-->
+                <t-content-card width="2">
+                    <template #title>
+                        With Color Options
+                    </template>
+                    <template #content>
+                        <div class="flex flex-wrap gap-4">
+                            <t-badge :radius="8" color="indigo">Indigo</t-badge>
+                            <t-badge :radius="8" color="red">Red</t-badge>
+                            <t-badge :radius="8" color="pink">Pink</t-badge>
+                            <t-badge :radius="8" color="yellow">Yellow</t-badge>
+                            <t-badge :radius="8" color="purple">Purple</t-badge>
+                            <t-badge :radius="8" color="white">White</t-badge>
+                            <t-badge :radius="8" color="black">Black</t-badge>
+                            <t-badge :radius="8" color="red">Red</t-badge>
+                            <t-badge :radius="8" color="blue">Blue</t-badge>
+                        </div>
+                    </template>
+                </t-content-card>
+                <!--With Value-->
+                <t-content-card width="2">
+                    <template #title>
+                        With Value Option
+                    </template>
+                    <template #content>
+                        <div class="flex flex-wrap gap-4">
+                            <t-badge :radius="8" color="indigo" value="123">Number</t-badge>
+                            <t-badge :radius="8" color="red" value="12 TL">Currency</t-badge>
+                            <t-badge :radius="8" color="pink" value="0,25">Double</t-badge>
+                            <t-badge :radius="8" color="yellow" value="15 pcs.">Count</t-badge>
+                            <t-badge :radius="8" color="purple" value="03.09.2025">Date</t-badge>
+                            <t-badge :radius="8" color="white" value="®">Special Char.</t-badge>
+                            <t-badge :radius="8" color="black" value="Plus">String</t-badge>
+                            <t-badge :radius="8" color="red" value="+25%">Percent</t-badge>
+                            <t-badge :radius="8" color="blue" value="-15px">Mathematical Signs</t-badge>
+                        </div>
+                    </template>
+                </t-content-card>
+            </grid-section>
+        </template>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import GridSection from "@/Layouts/GridSection";
+import TContentCard from "@/Components/Card/TContentCard";
+import TBadge from "@/Components/Badge/TBadge";
+export default {
+    name: "Badge",
+    components: {AppLayout,GridSection,TContentCard,TBadge}
+}
+</script>
+
+<style scoped>
+
+</style>
