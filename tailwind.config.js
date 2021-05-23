aba67c34c8d1fe3dfa5316adb5aed02463415ecd
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 12:44:29 +0300

    Tailwind scrollbar plugin added

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'),require('tailwind-scrollbar')],
 };
