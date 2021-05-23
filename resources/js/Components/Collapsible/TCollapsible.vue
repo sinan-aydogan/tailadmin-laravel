b23bb56c8d8f7f14fd8ebc61175bbc2664c131e2
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 12:42:10 +0300

    Collapsible component added

---
+++ resources/js/Components/Collapsible/TCollapsible.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="['flex flex-col w-full border border-gray-400 overflow-hidden',radiusStyle]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import {radiusSizeMixin} from "@/Mixins/radiusSizeMixin";
+
+export default {
+    name: "Collapsible",
+    mixins: [radiusSizeMixin]
+}
+</script>
+
+<style>
+/*
+.collapsible-border:first-child{
+  border-radius: 12px 12px 0 0;
+  border-top: 1px solid #9CA3AF;
+}*/
+.collapsible-border {
+    border-bottom: 1px solid #9CA3AF;
+}
+
+.collapsible-border:last-child {
+    border-bottom: 0 solid #9CA3AF;
+}
+
+</style>
---
+++ resources/js/Components/Collapsible/TCollapsibleItem.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex flex-col flex-wrap w-full overflow-hidden shadow-sm collapsible-border">
+        <!--Title-->
+        <div
+            :class="['flex w-full justify-between items-center p-4 font-bold cursor-pointer collapsible-sub-border',
+        showPanel && 'border-b border-gray-400',
+        color !== 'black'  ? 'bg-'+color+'-300' : 'text-gray-300 bg-'+color,
+        color === 'white' && 'text-gray-800 bg-white',
+        align === 'right' && 'flex-row-reverse']"
+            @click="showPanel = !showPanel">
+            <div
+                :class="['flex items-center gap-2 w-full',align === 'left' && 'justify-start',align === 'center' && 'justify-center',align === 'right' && 'flex-row-reverse']">
+                <slot name="title"></slot>
+            </div>
+            <div :class="[
+          color !== 'black'  ? 'text-'+color+'-800' : 'text-gray-300 bg-'+color,
+          color === 'white' && 'text-gray-800',
+      ]">
+                <font-awesome-icon icon="angle-down" size="lg"/>
+            </div>
+        </div>
+        <!--Content-->
+        <div v-if="showPanel" :class="['p-4 bg-'+color+'-50']">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CollapsibleItem",
+    props: {
+        color: {
+            type: String,
+            required: true,
+            default: 'gray'
+        },
+        align: {
+            type: String,
+            required: true,
+            default: 'left'
+        }
+    },
+    data() {
+        return {
+            showPanel: false
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.collapsible-border .collapsible-sub-border:not(:last-child) {
+}
+</style>
