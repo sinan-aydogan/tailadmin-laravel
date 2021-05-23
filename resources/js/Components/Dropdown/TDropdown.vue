003f04d6b3fb9e69f5fa93f090ecc9a57e10bb4b
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 12:41:10 +0300

    Dropdown component added

---
+++ resources/js/Components/Dropdown/TDropdown.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="max-w-min" v-click-outside="outside">
+    <div
+        @click="showContent = !showContent"
+        :class="colorStyle.trigger">
+      <slot name="title"></slot>
+      <font-awesome-icon icon="angle-down"/>
+    </div>
+    <div v-if="showContent" :class="colorStyle.childContent">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  directives: {
+    'click-outside': {
+      bind: function(el, binding, vNode) {
+        // Provided expression must evaluate to a function.
+        if (typeof binding.value !== 'function') {
+          const compName = vNode.context.name
+          let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
+          if (compName) { warn += `Found in component '${compName}'` }
+
+          console.warn(warn)
+        }
+        // Define Handler and cache it on the element
+        const bubble = binding.modifiers.bubble
+        const handler = (e) => {
+          if (bubble || (!el.contains(e.target) && el !== e.target)) {
+            binding.value(e)
+          }
+        }
+        el.__vueClickOutside__ = handler
+
+        // add Event Listeners
+        document.addEventListener('click', handler)
+      },
+
+      unbind: function(el) {
+        // Remove Event Listeners
+        document.removeEventListener('click', el.__vueClickOutside__)
+        el.__vueClickOutside__ = null
+
+      }
+    }
+  },
+  props : {
+    color : {
+      type : String,
+      default : 'white'
+    }
+  },
+  data(){
+    return {
+      showContent : false,
+      colorStyle : {
+        trigger : '',
+        childContent : ''
+      }
+    }
+  },
+  methods : {
+    outside(){
+      this.showContent = false
+    }
+  },
+  created(){
+    if(this.color === 'white'){
+      this.colorStyle.trigger = 'relative flex flex-row justify-between border rounded-md p-2 items-center gap-2 cursor-pointer'
+    }else if(this.color === 'black'){
+      this.colorStyle.trigger = 'relative flex flex-row justify-between border rounded-md p-2 items-center gap-2 cursor-pointer'
+    }else{
+      this.colorStyle.trigger = 'relative bg-'+this.color+'-500 text-red-100 flex flex-row justify-between border rounded-md p-2 items-center gap-2 cursor-pointer',
+          this.colorStyle.childContent = 'absolute border rounded-md mt-1 overflow-hidden bg-'+this.color+'-200'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Dropdown/TDropdownItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-2 hover:bg-gray-600 hover:text-white cursor-pointer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropdownItem"
+}
+</script>
+
+<style scoped>
+
+</style>
