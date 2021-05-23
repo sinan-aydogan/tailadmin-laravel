13c4e54cebf9979347be6273bc4d922259f2ab00
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 21:46:27 +0300

    Fontawesome icons reordered

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,39 +8,16 @@ import PortalVue from 'portal-vue';
 // Fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-    faRocket,
-    faDotCircle,
-    faWindowRestore,
-    faEdit,
-    faCalendarAlt,
-    faEnvelope,
-    faTasks,
-    faWindowMaximize,
-    faChartBar,
-    faChevronDown,
-    faCode,
-    faAngleDown,
-    faAngleUp,
-    faLayerGroup,
-    faGripHorizontal,
-    faAngleLeft,
-    faClock,
-    faRetweet,
-    faTags,
-    faTv,
-    faPlug,
-    faTable, faShoppingBag, faLiraSign, faTrash, faPlusCircle, faAngleDoubleRight
+import {faRocket,faDotCircle,faWindowRestore,faEdit,faCalendarAlt,faEnvelope,faTasks,faWindowMaximize,faChartBar,faChevronDown,faCode,faAngleDown,faAngleUp,faLayerGroup,faGripHorizontal,faAngleLeft,faClock,faRetweet,faTags,faTv,faPlug,faTable,faShoppingBag,faLiraSign,faTrash,faPlusCircle,faAngleDoubleRight
 } from '@fortawesome/free-solid-svg-icons'
-library.add(faRocket, faDotCircle, faWindowRestore, faEdit, faCalendarAlt,faEnvelope,faTasks,faWindowMaximize,faChartBar,faChevronDown,faCode,faAngleDown,faAngleUp,faChartBar,faLayerGroup,faGripHorizontal,faAngleLeft,faClock,faRetweet,faTags,faTv,faPlug,faTable,faShoppingBag,faLiraSign,faTrash,faPlusCircle,faAngleDoubleRight)
-
-
+library.add(faRocket,faDotCircle,faWindowRestore,faEdit,faCalendarAlt,faEnvelope,faTasks,faWindowMaximize,faChartBar,faChevronDown,faCode,faAngleDown,faAngleUp,faChartBar,faLayerGroup,faGripHorizontal,faAngleLeft,faClock,faRetweet,faTags,faTv,faPlug,faTable,faShoppingBag,faLiraSign,faTrash,faPlusCircle,faAngleDoubleRight)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
+
 
 const app = document.getElementById('app');
 
