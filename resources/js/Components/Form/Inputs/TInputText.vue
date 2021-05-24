16f3927b4de139722a1e66e9671cff8b269e6e80
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-25 01:39:19 +0300

    Text Input: Placeholder option added

---
+++ resources/js/Components/Form/Inputs/TInputText.vue
@@ -0,0 +1,14 @@
+<template>
+        <input class="form-input h-10" type="text" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+</template>
+
+<script>
+export default {
+    name: "InputText",
+    props: ['value','id', 'placeholder']
+}
+</script>
+
+<style scoped>
+
+</style>
