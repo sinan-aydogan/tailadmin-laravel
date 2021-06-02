3b2c2be360c5a27365f2af9215f504a5f9ff10d3
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-06-02 14:34:57 +0300

    Empty record notification and HTML content allow

--- resources/js/Components/Table/TTable.vue
+++ resources/js/Components/Table/TTable.vue
@@ -2,27 +2,38 @@
   <div class="rounded-md overflow-hidden shadow-lg">
     <table class="w-full">
       <thead>
-        <tr :class="'bg-'+color+'-200'">
-          <th v-for="(item,index) in header" :key="index" class="p-2">{{ item.toUpperCase() }}</th>
-        </tr>
+      <tr :class="headerStyle">
+        <th v-for="(item,index) in header" :key="index" :class="['p-2',item.align ? 'text-'+item.align : 'text-left']">{{ item.label }}</th>
+      </tr>
       </thead>
       <tbody>
-        <tr
-            v-for="(item,index) in content"
-            :key="index"
-            :class="['hover:bg-'+color+'-200',index%2 === 0 ? 'bg-white' : 'bg-'+color+'-100']">
-          <td v-for="i in header.length" :key="i" class="p-4">
-            <span v-if="!$scopedSlots[header[i-1]]">{{ item[header[i-1]] }}</span>
-            <slot v-else :name="header[i-1]" :props="item"></slot>
-
-          </td>
-        </tr>
+      <tr v-if="content.length === 0" >
+        <td :colspan="header.length" :class="noContentStyle">
+          <div class="noContentStyle">
+            <font-awesome-icon icon="info-circle"/>
+            Any record not found
+          </div>
+        </td>
+      </tr>
+      <tr
+          v-for="(item,index) in content"
+          :key="index"
+          :class="['hover:bg-'+color+'-200',index%2 === 0 ? 'bg-white' : 'bg-'+color+'-100']">
+        <td v-for="i in header.length" :key="i" :class="['p-4',header[i-1].align ? 'text-'+header[i-1].align : 'text-left']">
+          <span v-if="!$scopedSlots[header[i-1].key]" v-html="item[header[i-1].key]"></span>
+          <slot v-else :name="header[i-1].key" :props="item"></slot>
+        </td>
+      </tr>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons'
+library.add(faInfoCircle)
+
 export default {
   name: "Table",
   props : {
@@ -33,12 +44,32 @@ export default {
       type : Array
     },
     color : {
-      type: String
+      type: String,
+      default: 'white'
     }
   },
-  data(){
-    return {
-
+  computed: {
+    headerStyle(){
+      let headerStyle;
+      if(this.color === 'white'){
+        headerStyle = 'bg-white'
+      }else if(this.color === 'black'){
+        headerStyle = 'bg-black text-white'
+      }else{
+        headerStyle = 'bg-'+this.color+'-200'
+      }
+      return headerStyle
+    },
+    noContentStyle(){
+      let noContentStyle;
+      if(this.color === 'white'){
+        noContentStyle = 'bg-red-200 text-gray-600 text-center p-3'
+      }else if(this.color === 'black'){
+        noContentStyle = 'bg-gray-500 text-gray-200 text-center p-3'
+      }else{
+        noContentStyle = 'bg-'+this.color+'-50  text-'+this.color+'-500 text-center p-3'
+      }
+      return noContentStyle
     }
   }
 }
