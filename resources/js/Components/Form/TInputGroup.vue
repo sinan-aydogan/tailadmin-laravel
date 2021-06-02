73b8dd22430ce5fffa65f90657e4eb326f71ab8c
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-06-02 14:35:34 +0300

    Colorful Sub Label feature added

--- resources/js/Components/Form/TInputGroup.vue
+++ resources/js/Components/Form/TInputGroup.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="flex flex-col">
-        <!--Label-->
-        <label :for="labelFor" class="block font-medium text-sm text-gray-700">
-            <span v-if="label">{{ label }}</span>
-            <span v-else><slot #label></slot></span>
-        </label>
-        <!--Input Content-->
-        <slot></slot>
-        <!--Error-->
-        <div v-show="error">
-            <p class="text-sm text-red-600 mt-2">
-                {{ error }}
-            </p>
-        </div>
+  <div class="flex flex-col w-full">
+    <!--Label-->
+    <label :for="labelFor" class="block font-medium text-sm text-gray-700 whitespace-normal">
+            <span v-if="label">
+              {{ label }}
+              <span v-if="subLabel" :class="'text-'+subLabelColor+'-500 text-xs italic'">{{ subLabel }}</span>
+            </span>
+      <span v-else><slot #label></slot></span>
+    </label>
+    <!--Input Content-->
+    <slot></slot>
+    <!--Error-->
+    <div v-show="error">
+      <p class="text-sm text-red-600 mt-2 whitespace-nowrap">
+        {{ error }}
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['label','labelFor','error']
+  props: ['label', 'subLabel', 'subLabelColor', 'labelFor', 'error'],
 }
 </script>
