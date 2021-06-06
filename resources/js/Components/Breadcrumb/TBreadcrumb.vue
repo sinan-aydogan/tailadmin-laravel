574daa833dafd4d4e04bf0e9fd5030f85e93c106
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-06-06 13:00:31 +0300

    Whitespace error fixed

--- resources/js/Components/Breadcrumb/TBreadcrumb.vue
+++ resources/js/Components/Breadcrumb/TBreadcrumb.vue
@@ -10,41 +10,43 @@
             v-for="(item,index) in breadcrumbs"
             :key="index"
             :class="['flex items-center justify-center gap-2',
-        breadcrumbs.length !== index+1 ?
-        'font-semibold text-gray-600' :
-                item.activeColor ?
+            breadcrumbs.length !== index+1 ?
+            'font-semibold text-gray-600' :
+              item.activeColor ?
                 'border-2 px-2 py-1 rounded-full font-bold border-'+item.activeColor+'-600 text-'+item.activeColor+'-600' :
-                'border-2 px-2 py-1 rounded-full font-bold border-blue-600 text-blue-600'
+                  'border-2 px-2 py-1 rounded-full font-bold border-blue-600 text-blue-600'
         ]">
           {{ item.label }}
           <font-awesome-icon v-if="breadcrumbs.length !== index+1" class="mt-0.5" icon="angle-double-right"/>
         </div>
       </div>
-      <span v-if="$slots.hasOwnProperty('subContent')" class="px-3 text-2xl text-gray-700">
+      <div v-if="$slots.hasOwnProperty('subContent')" class="px-3 text-2xl text-gray-700">
         <slot name="subContent"></slot>
-      </span>
+      </div>
     </div>
     <!--Style 2-->
     <div
         v-if="breadcrumbStyle === 2"
-        :class="['hidden sm:flex flex-wrap items-center justify-between gap-1 bg-white bg-opacity-50 border-b-4 shadow-sm', position === 'right' ? 'flex-row-reverse' : 'justify-end']">
+        :class="['hidden sm:flex flex-wrap items-center justify-between gap-1 bg-white bg-opacity-50 border-b-4 shadow-sm',
+        position === 'right' ? 'flex-row-reverse' : 'justify-end'
+        ]">
       <div class="flex flex-row gap-1">
         <div
             v-for="(item,index) in breadcrumbs"
             :key="index"
             :class="[
               'flex items-center justify-center p-3 border-b-4 -mb-1',
-              breadcrumbs.length !== index+1 ? 'bg-gray-300 border-gray-500' :
-              'bg-'+item.activeColor+'-500 border-'+item.activeColor+'-900 bg-opacity-80 font-bold text-white',
+                breadcrumbs.length !== index+1 ? 'bg-gray-300 border-gray-500' :
+                  'bg-'+item.activeColor+'-500 border-'+item.activeColor+'-900 bg-opacity-80 font-bold text-white',
               ]">
-        <span class="flex">
-          {{item.label}}
-        </span>
+        <div class="flex">
+          {{ item.label }}
+        </div>
         </div>
       </div>
-      <span v-if="$slots.hasOwnProperty('subContent')" class="px-3 text-2xl text-gray-700">
+      <div v-if="$slots.hasOwnProperty('subContent')" class="px-3 text-2xl text-gray-700">
         <slot name="subContent"></slot>
-      </span>
+      </div>
     </div>
     <!--Mobile-->
   </div>
@@ -52,20 +54,20 @@
 
 <script>
 export default {
-  props : {
-    breadcrumbStyle : {
-      type : Number,
+  props: {
+    breadcrumbStyle: {
+      type: Number,
       require: true,
-      default :1
+      default: 1
     },
-    position : {
+    position: {
       type: String,
       require: true,
       default: 'left'
     },
-    breadcrumbs : {
+    breadcrumbs: {
       type: Array,
-      require : true
+      require: true
     }
   },
   name: "Breadcrumb"
