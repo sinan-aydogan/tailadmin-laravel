c40a1833d5d3fc542a4a8cfc85a2e6a66478fc70
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-06-16 00:18:23 +0300

    Popover component and documentation added

--- resources/js/Pages/Samples/Components/Popover.vue
+++ resources/js/Pages/Samples/Components/Popover.vue
@@ -3,35 +3,101 @@
     <template #header>Popovers</template>
     <template #subHeader>Simple and casual pops</template>
     <template #default>
-
-      <ssh-pre copy-button="true" language="html" label="Code">{{sampleCode.html}}</ssh-pre>
-      <ssh-pre copy-button="true" language="js" label="JS">{{sampleCode.js}}</ssh-pre>
+        <grid-section col="2" gap="4">
+            <content-card width="1">
+                <template #title>Popover Title and Position</template>
+                <template #subTitle>Left, Top, Right and Bottom Positions</template>
+                <template #content>
+                    <div class="flex flex-wrap gap-8">
+                        <t-popover position="top">
+                            <template #mainContent>Click me!</template>
+                            <template #secondTitle>Title for Top</template>
+                            <template #secondContent>Hello, I'm at the top</template>
+                        </t-popover>
+                        <t-popover position="bottom">
+                            <template #mainContent>Click me!</template>
+                            <template #secondTitle>Title for Bottom</template>
+                            <template #secondContent>Hello, I'm at the bottom</template>
+                        </t-popover>
+                        <t-popover position="left">
+                            <template #mainContent>Click me!</template>
+                            <template #secondTitle>Title for Left</template>
+                            <template #secondContent>Hello, I'm at the left</template>
+                        </t-popover>
+                        <t-popover position="right">
+                            <template #mainContent>Click me!</template>
+                            <template #secondTitle>Title for Right</template>
+                            <template #secondContent>Hello, I'm at the right</template>
+                        </t-popover>
+                    </div>
+                </template>
+            </content-card>
+            <content-card width="1">
+                <template #title>Popover without Title</template>
+                <template #subTitle>Left, Top, Right and Bottom Positions</template>
+                <template #content>
+                    <div class="flex flex-wrap gap-8">
+                        <t-popover position="top">
+                            <template #mainContent>Click me!</template>
+                            <template #secondContent>Hello, I'm at the top</template>
+                        </t-popover>
+                        <t-popover position="bottom">
+                            <template #mainContent>Click me!</template>
+                            <template #secondContent>Hello, I'm at the bottom</template>
+                        </t-popover>
+                        <t-popover position="left">
+                            <template #mainContent>Click me!</template>
+                            <template #secondContent>Hello, I'm at the left</template>
+                        </t-popover>
+                        <t-popover position="right">
+                            <template #mainContent>Click me!</template>
+                            <template #secondContent>Hello, I'm at the right</template>
+                        </t-popover>
+                    </div>
+                </template>
+            </content-card>
+        </grid-section>
+        <!--Sample Codes-->
+        <ssh-pre :copy-button="true" label="Code" language="html">{{ sampleCode.html }}</ssh-pre>
+        <ssh-pre :copy-button="true" label="JS" language="js">{{ sampleCode.js }}</ssh-pre>
+        <!--Variables Table-->
+        <t-table :content="sampleCode.table.content" :header="sampleCode.table.header" class="mt-5" color="blue"/>
     </template>
   </app-layout>
 </template>
 
 <script>
+/*Layout*/
 import AppLayout from "@/Layouts/AppLayout";
-import SshPre from 'simple-syntax-highlighter'
-import 'simple-syntax-highlighter/dist/sshpre.css'
+/*Component*/
+import GridSection from "@/Layouts/GridSection";
+import TButton from "@/Components/Button/TButton";
+import TContentCard from "@/Components/Card/TContentCard";
+import TTable from "@/Components/Table/TTable";
+import TPopover from "@/Components/Popover/TPopover";
+/*Codehighlighter*/
+import "simple-syntax-highlighter/dist/sshpre.css";
+import SshPre from "simple-syntax-highlighter";
+import ContentCard from "@/Components/Card/TContentCard";
 
 export default {
   name: "Popover",
-  components: {AppLayout, SshPre},
+  components: {ContentCard, AppLayout, SshPre,GridSection,TButton,TContentCard,TTable,TPopover},
   data() {
     return {
       sampleCode: {
         html:
-            '<t-input-group label="Text Label" label-for="name4" sub-label="Cash only" sub-label-color="yellow" error="Please enter numerical value">\n' +
-            '    <t-input-text id="name4" placeholder="I\'m a placeholder text" value="Hi, I\'m a prefilled text"/>\n' +
-            '</t-input-group>',
+            '<t-popover position="right">\n' +
+            '    <template #mainContent>Click me!</template>\n' +
+            '    <template #secondTitle>Title for Right</template>\n' +
+            '    <template #secondContent>Hello, I\'m at the right</template>\n' +
+            '</t-popover>',
         js:
-            'import TInputGroup from "@/Components/Form/TInputGroup";\n' +
-            'import TInputText from "@/Components/Form/Inputs/TInputText";\n' +
+            'import TPopover from "@/Components/Popover/TPopover";\n' +
             '\n' +
             'export default {\n' +
-            '  name: "InputGroup",\n' +
-            '  components: {SshPre,GridSection, TInputText, TInputGroup, ContentCard, AppLayout},\n' +
+            '  name: "Popover",\n' +
+            '  components: {TPopover},\n' +
             '  }',
         table: {
           header: [
@@ -40,22 +106,10 @@ export default {
             {key: 'details', label: 'Details'},
           ],
           content: [
-            {variable: 'label', type: 'String', details: 'Your input label'},
-            {variable: 'label-for', type: 'String', details: 'Label identity indicator, you should enter input\'s id'},
             {
-              variable: 'sub-label',
+              variable: 'position',
               type: 'String',
-              details: 'If you want to show a second and small label, you can use'
-            },
-            {
-              variable: 'sub-label-color',
-              type: 'String',
-              details: 'You can change sub label color <br><b>Options:</b> red, blue, green, yellow, indigo, pink, gray'
-            },
-            {
-              variable: 'error',
-              type: 'String',
-              details: 'Every time it\'s text color is red, If It has a value, it shows'
+              details: '<b>Options:</b> left, right, bottom and top'
             }
           ]
         }
