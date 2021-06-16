6621e63528f1fd46b4b87af03ff8061025374118
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-06-16 03:31:10 +0300

    Progress bar component and documentation added

---
+++ resources/js/Components/Progress/TProgress.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="flex flex-col w-full">
+        <div class="flex flex-row justify-between text-sm">
+            <div v-text="title"></div>
+            <div>{{ counter + '%' }}</div>
+        </div><transition name="progress">
+            <div :class="[
+            'flex w-full bg-gray-200 shadow-inner rounded-full overflow-hidden flex-shrink-0 flex-grow-0',
+            6>height ? 'h-'+height : 'h-2'
+            ]">
+                <transition appear @before-enter="beforeStyle" @after-appear="enterStyle">
+                <div :class="[
+                    'flex',
+                    colorStyle,
+                    6>height ? 'h-'+height : 'h-2',
+                    ]"
+                ></div>
+                </transition>
+
+    </div></transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TProgress",
+    props: {
+        title: {
+            type: String
+        },
+        value: {
+            type: Number,
+            default: 10
+        },
+        color: {
+            type: String,
+            default: 'green'
+        },
+        height: {
+            type: Number,
+            default: 3
+        }
+    },
+    data(){
+        return {
+            counterValue:0
+        }
+    },
+    computed: {
+        colorStyle() {
+            if (this.color === 'white') {
+                return 'bg-white'
+            } else if (this.color === 'black') {
+                return 'bg-black'
+            } else {
+                return 'bg-' + this.color + '-500'
+            }
+        },
+        counter(){
+            let vm = this;
+            if(vm.value>100){
+                vm.value = 100
+            }
+            if(vm.counterValue>vm.value){
+                clearInterval(this.fn())
+            }
+            let fn = setInterval(()=>{
+                vm.counterValue++
+            },280)
+            return vm.counterValue
+        }
+    },
+    methods: {
+        beforeStyle(event){
+            event.style.width = 0
+        },
+        enterStyle(event){
+            if(this.value>100){
+                this.value = 100
+            }
+            event.style.width = this.value+'%'
+            event.style.transition = `all 2s linear`
+        }
+    },
+}
+</script>
--- resources/js/Pages/Samples/Components/Progress.vue
+++ resources/js/Pages/Samples/Components/Progress.vue
@@ -1,67 +1,92 @@
 <template>
-  <app-layout>
-    <template #header>Progress Bars</template>
-    <template #subHeader>Loading, graphical indicator and more than</template>
-    <template #default>
-
-      <ssh-pre copy-button="true" language="html" label="Code">{{sampleCode.html}}</ssh-pre>
-      <ssh-pre copy-button="true" language="js" label="JS">{{sampleCode.js}}</ssh-pre>
-    </template>
-  </app-layout>
+    <app-layout>
+        <template #header>Progress Bars</template>
+        <template #subHeader>Loading, graphical indicator and more than</template>
+        <template #default>
+            <grid-section col="1">
+                <t-content-card width="1">
+                    <template #title>Simple and colorful</template>
+                    <template #subTitle>8 different color and 5 different height</template>
+                    <template #content>
+                        <div class="flex flex-col w-full gap-4">
+                            <t-progress :height="1" :value="5" title="Performance"/>
+                            <t-progress :height="2" :value="25" color="red" title="Claim"/>
+                            <t-progress :height="3" :value="50" color="yellow" title="Cost"/>
+                            <t-progress :height="4" :value="75" color="blue" title="Man Hour"/>
+                            <t-progress :height="5" :value="90" color="indigo" title="Lost"/>
+                        </div>
+                    </template>
+                </t-content-card>
+            </grid-section>
+            <!--Sample Codes-->
+            <ssh-pre :copy-button="true" label="Code" language="html">{{ sampleCode.html }}</ssh-pre>
+            <ssh-pre :copy-button="true" label="JS" language="js">{{ sampleCode.js }}</ssh-pre>
+            <!--Variables Table-->
+            <t-table :content="sampleCode.table.content" :header="sampleCode.table.header" class="mt-5" color="blue"/>
+        </template>
+    </app-layout>
 </template>
 
 <script>
+/*Layout*/
 import AppLayout from "@/Layouts/AppLayout";
-import SshPre from 'simple-syntax-highlighter'
-import 'simple-syntax-highlighter/dist/sshpre.css'
+/*Component*/
+import GridSection from "@/Layouts/GridSection";
+import TContentCard from "@/Components/Card/TContentCard";
+import TProgress from "@/Components/Progress/TProgress";
+import TTable from "@/Components/Table/TTable";
+/*Codehighlighter*/
+import SshPre from "simple-syntax-highlighter";
+import "simple-syntax-highlighter/dist/sshpre.css";
 
 export default {
-  name: "Progress",
-  components: {AppLayout, SshPre},
-  data() {
-    return {
-      sampleCode: {
-        html:
-            '<t-input-group label="Text Label" label-for="name4" sub-label="Cash only" sub-label-color="yellow" error="Please enter numerical value">\n' +
-            '    <t-input-text id="name4" placeholder="I\'m a placeholder text" value="Hi, I\'m a prefilled text"/>\n' +
-            '</t-input-group>',
-        js:
-            'import TInputGroup from "@/Components/Form/TInputGroup";\n' +
-            'import TInputText from "@/Components/Form/Inputs/TInputText";\n' +
-            '\n' +
-            'export default {\n' +
-            '  name: "InputGroup",\n' +
-            '  components: {SshPre,GridSection, TInputText, TInputGroup, ContentCard, AppLayout},\n' +
-            '  }',
-        table: {
-          header: [
-            {key: 'variable', label: 'Variable'},
-            {key: 'type', label: 'Value Type'},
-            {key: 'details', label: 'Details'},
-          ],
-          content: [
-            {variable: 'label', type: 'String', details: 'Your input label'},
-            {variable: 'label-for', type: 'String', details: 'Label identity indicator, you should enter input\'s id'},
-            {
-              variable: 'sub-label',
-              type: 'String',
-              details: 'If you want to show a second and small label, you can use'
-            },
-            {
-              variable: 'sub-label-color',
-              type: 'String',
-              details: 'You can change sub label color <br><b>Options:</b> red, blue, green, yellow, indigo, pink, gray'
-            },
-            {
-              variable: 'error',
-              type: 'String',
-              details: 'Every time it\'s text color is red, If It has a value, it shows'
+    name: "Progress",
+    components: {TProgress, TContentCard, GridSection, AppLayout, SshPre, TTable},
+    data() {
+        return {
+            sampleCode: {
+                html:
+                    '<t-progress title="Man Hour" :value="75" color="blue" :height="4"/>',
+                js:
+                    'import TProgress from "@/Components/Progress/TProgress";' +
+                    '\n' +
+                    'export default {\n' +
+                    '  name: "Progress",\n' +
+                    '  components: {TProgress},\n' +
+                    '  }',
+                table: {
+                    header: [
+                        {key: 'variable', label: 'Variable'},
+                        {key: 'type', label: 'Value Type'},
+                        {key: 'details', label: 'Details'},
+                    ],
+                    content: [
+                        {
+                            variable: 'title',
+                            type: 'String',
+                            details: 'You want to what ever'
+                        },
+                        {
+                            variable: ':value',
+                            type: 'Number',
+                            details: 'It must be between 1 and 100'
+                        },
+                        {
+                            variable: "color",
+                            type: "String",
+                            details: "Your progress bar color theme.<br><b>Options:</b> red,blue,green,yellow,indigo,pink,purple,gray,black,white. <b>Default:</b> green"
+                        },
+
+                        {
+                            variable: ":height",
+                            type: "Number",
+                            details: "It's line height <br><b>Options:</b> 1,2,3,4,5. <b>Default:</b> 3"
+                        }
+                    ]
+                }
             }
-          ]
         }
-      }
     }
-  }
 }
 </script>
 
