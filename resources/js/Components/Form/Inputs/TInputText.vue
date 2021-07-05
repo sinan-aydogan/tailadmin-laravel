b6e77461ec6d2fd25695ecd25f529d2331f1b36b
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-05 04:24:28 +0300

    Input type prop added

--- resources/js/Components/Form/Inputs/TInputText.vue
+++ resources/js/Components/Form/Inputs/TInputText.vue
@@ -3,9 +3,9 @@
         <div :class="['absolute flex w-10 h-10 bg-gray-200 items-center justify-center rounded-r-none border-gray-300 border border-r-0 text-gray-600', radiusStyle]">
             <slot name="icon"></slot>
         </div>
-        <input :class="['form-input rounded-l-none h-10 ml-10',radiusStyle]" type="text" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+        <input :class="['form-input rounded-l-none h-10 ml-10',radiusStyle]" :type="type" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input">
     </div>
-    <input v-else :class="['form-input h-10', radiusStyle]" type="text" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+    <input v-else :class="['form-input h-10', radiusStyle]" :type="type" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input">
 
 </template>
 
@@ -13,7 +13,15 @@
 import {radiusSizeMixin} from "@/Mixins/radiusSizeMixin";
 export default {
     name: "TInputText",
-    props: ['value','id', 'placeholder'],
+    props: {
+      value:{},
+      id:{},
+      placeholder: {},
+      type: {
+        default: 'text'
+      }
+
+},
     mixins: [radiusSizeMixin],
 }
 </script>
