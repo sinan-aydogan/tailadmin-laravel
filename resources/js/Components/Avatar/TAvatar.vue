74b0c5bda9bacc0f486339bac31f9d54e95aa76f
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-05 04:42:29 +0300

    Team prop deleted

--- resources/js/Components/Avatar/TAvatar.vue
+++ resources/js/Components/Avatar/TAvatar.vue
@@ -1,17 +1,17 @@
 <template>
-    <div :class="[team === true ? '-ml-4 overflow-hidden' : 'relative flex']">
-        <img :class="[radiusStyle, sizes[size], team === true ? 'border-2 border-white border-opacity-50' : '']"
+    <div :class="[$parent.$vnode.tag.includes('TAvatarGroup') && '-ml-4 hover:absolute hover:z-10' ,'relative z-0',sizes[size]]">
+        <img :class="[radiusStyle, sizes[size], $parent.$vnode.tag.includes('TAvatarGroup') ? 'border-2 border-white border-opacity-75' : '']"
              :src="avatarURL">
-        <span
+        <div
             v-if="indicator"
             :class="[
-                'absolute flex justify-center items-center w-1/3 h-1/3 rounded-full text-white font-semibold',
+                'absolute flex flex-shrink-0 justify-center items-center w-1/3 h-1/3 rounded-full text-white font-semibold',
                 size<7 ? 'text-xs' : 'text-normal',
                 positions[indicator.position].positionStyle,
                 'bg-'+indicator.color+'-500'
                 ]">
-            <span v-text="size>3 ? indicator.label : null"/>
-        </span>
+            <div v-text="size>3 ? indicator.label : null"/>
+        </div>
     </div>
 </template>
 
@@ -26,10 +26,6 @@ export default {
         src: {
             type: String,
             require: false,
-        },
-        team: {
-            type: Boolean,
-            require: false
         }
     },
     computed: {
@@ -42,6 +38,6 @@ export default {
             }
             return imgSrc
         }
-    }
+    },
 };
 </script>
