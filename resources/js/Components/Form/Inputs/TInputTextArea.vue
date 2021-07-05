7302153c450b2abf68f5baa34ff7180ef66e36d6
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-05 04:23:46 +0300

    Radius feature added, clear method changed

--- resources/js/Components/Form/Inputs/TInputTextArea.vue
+++ resources/js/Components/Form/Inputs/TInputTextArea.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="relative">
-    <span v-if="clearButton && value" class="absolute flex -top-3 -right-3 border w-6 h-6 items-center justify-center rounded-lg bg-white hover:bg-red-500 hover:text-white cursor-pointer" @click="value = null">x</span>
-    <textarea class="form-input h-auto w-full" :rows="rows" :id="id" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" ref="input"></textarea>
-    <span class="absolute bg-white bg-opacity-70 bottom-3 right-2 float-right text-xs border px-1 py-0.5 rounded-full min-w-6 text-center bg-white-500" v-if="counter && value">{{value.length}}</span>
+    <t-x-circle-icon
+        v-if="clearButton && value"
+        @click.native="value = null"
+        class="absolute rounded-full -top-3 -right-3 w-6 h-6 bg-white hover:bg-red-500 text-red-500 hover:text-white cursor-pointer"
+    />
+    <textarea
+        :class="['form-input h-auto w-full',radiusStyle]"
+        :rows="rows"
+        :id="id"
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        ref="input"
+    />
+    <span
+        class="absolute bg-white bg-opacity-70 bottom-3 right-2 float-right text-xs border px-1 py-0.5 rounded-full min-w-6 text-center bg-white-500"
+        v-if="counter && value">
+      {{value.length}}
+    </span>
   </div>
 </template>
 
 <script>
+import {radiusSizeMixin} from "@/Mixins/radiusSizeMixin";
+import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
+
 export default {
-  name: "InputTextArea",
+  name: "TInputTextArea",
+  components: {TXCircleIcon},
+  mixins: [radiusSizeMixin],
   props: {
     value: {
       type: String
