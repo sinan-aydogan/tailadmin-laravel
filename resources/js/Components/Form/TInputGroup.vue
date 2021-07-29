79ab721f197741a4af1e539fca36910485291f11
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-30 02:59:49 +0300

    Input Group: Style Improvements (Purge Problem Solved) -Simple colors constants changed: red -> solid-red -Label prop and Label Slot split from each other

--- resources/js/Components/Form/TInputGroup.vue
+++ resources/js/Components/Form/TInputGroup.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="flex flex-col">
     <!--Label-->
-    <label :for="labelFor" class="block font-medium text-sm text-gray-700 whitespace-normal">
-            <span v-if="label">
-              {{ label }}
-              <span v-if="subLabel" :class="'text-'+subLabelColor+'-500 text-xs italic'">{{ subLabel }}</span>
-            </span>
-      <span v-if="!label && $slots.label"><slot #label></slot></span>
+    <label
+        :for="labelFor"
+        class="flex flex-row gap-1 items-center font-medium text-sm text-gray-700 whitespace-normal"
+    >
+      <!--Label-->
+      <span v-if="label">
+        {{ label }}
+      </span>
+      <!--Label Prop-->
+      <span v-if="!label && $slots.label" class="inline-flex items-center">
+        <!--Label Slot-->
+        <slot name="label"/>
+      </span>
+      <!--Sub Label-->
+      <span
+          v-if="subLabel"
+          :class="['text-xs italic',subLabelColors[subLabelColor]]"
+      >
+        {{ subLabel }}
+      </span>
     </label>
     <!--Input Content-->
     <slot></slot>
@@ -20,7 +34,10 @@
 </template>
 
 <script>
+import {inputGroupStyleMixin} from "@/Mixins/Styles/inputGroupStyleMixin";
+
 export default {
   props: ['label', 'subLabel', 'subLabelColor', 'labelFor', 'error'],
+  mixins: [inputGroupStyleMixin]
 }
 </script>
