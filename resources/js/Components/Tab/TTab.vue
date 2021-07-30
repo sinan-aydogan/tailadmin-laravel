c87d8f6f1a3533f47ae6bff3199a280e2ac42394
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-30 03:36:35 +0300

    Tab: Style Improvements (Purge Problem Solved) -Simple colors constants changed: red -> solid-red - Tab designs revised to more smart

--- resources/js/Components/Tab/TTab.vue
+++ resources/js/Components/Tab/TTab.vue
@@ -1,104 +1,69 @@
 <template>
-    <div class="flex flex-col w-full ">
-        <div
-            :class="[
-            'flex flex-row max-w-min',
-            tabStyle === 2 && 'p-2 gap-2 rounded-b-0 rounded-t'+radiusStyle,
-            tabStyle === 2 & color === 'black' && 'bg-black',
-            tabStyle === 2 & color === 'white' && 'bg-gray-200',
-            tabStyle === 2 & color !== 'black' && 'bg-'+color+'-500',
+  <div class="tab ">
+    <!--Title Container-->
+    <div
+        :class="[
+            'tab-title-container',
+            tabStyle === 2 && calculatedTitle2ContainerStyle,
             ]">
-            <div
-                v-for="(item,index) in tabs"
-                :key="index"
-                :class="['flex justify-start cursor-pointer font-semibold whitespace-normal',
-              tabStyle === 1 ?
-                 'border border-b-0 px-4 py-2' :
-                 'border-0 px-4 py-1 rounded'+radiusStyle,
-               color !== 'black'  ? 'bg-'+color+'-300'  : 'text-gray-200 bg-'+color,
-               color === 'black' & !item.isActive && 'text-gray-700',
-               color === 'white' && 'text-gray-800 bg-gray-200',
-               item.isActive ?
-                  color !== 'white' ? 'bg-opacity-90 text-'+color+'-900' : 'border border-gray-300' :
-                 tabStyle === 1 ? 'bg-opacity-20 text-gray-400' : 'bg-'+color+'-500 text-white',
-               index === tabs.length-1 & tabStyle === 1 && 'rounded-tr'+radiusStyle,
-               index === 0 & tabStyle === 1 && 'rounded-tl'+radiusStyle
-          ]" @click="selectedTab(index)">
-                {{item.title}}
-            </div>
-        </div>
-        <div :class="[
-        'flex p-2 border rounded-r'+radiusStyle+' rounded-bl'+radiusStyle,
-        color !== 'black'  ? 'bg-'+color+'-50 text-'+color+'-900' : 'text-gray-200 bg-'+color,
-        color === 'white' && 'text-gray-800 bg-white',
-        ]">
-            <slot/>
-        </div>
+      <!--Title-->
+      <div
+          v-for="(item,index) in tabs"
+          :key="index"
+          :class="[
+              'tab-title',
+              tabStyle === 1 ? 'tab-title-1' : 'tab-title-2 '+ radiusStyles[radius].title2,
+              item.isActive ? calculatedActiveTitleStyle : calculatedTitleStyle,
+              index === 0 & tabStyle === 1 && radiusStyles[radius].title1left,
+              index === tabs.length-1 & tabStyle === 1 && radiusStyles[radius].title1right
+          ]"
+          @click="selectedTab(index)"
+      >
+        {{ item.title }}
+      </div>
     </div>
+      <div :class="[
+        'tab-content',
+        calculatedContentStyle
+        ]">
+        <slot/>
+      </div>
+  </div>
 </template>
 
 <script>
+import {tabStyleMixin} from "@/Mixins/Styles/tabStyleMixin";
+
 export default {
-    name: 'TTab',
-    props: {
-        radius: {
-            required: false,
-            default: 0
-        },
-        color: {
-            required: false,
-            default: 'white'
-        },
-        tabStyle: {
-            required: false,
-            default: 1
-        },
-        active: {
-            type: Number,
-            default: 1
-        }
-    },
-    data() {
-        return {
-            activeID: null,
-            tabs: [],
-            isActive: false,
-            radiusSizes: [
-                '-sm',
-                '',
-                '-md',
-                '-lg',
-                '-xl',
-                '-2xl',
-                '-3xl',
-                '-full'
-            ]
-        }
-    },
-    created() {
-        this.tabs = this.$children
-    },
-    mounted() {
-        this.selectedTab(this.active-1)
-    },
-    methods: {
-        selectedTab(ID) {
-            this.activeID = ID;
-            this.tabs.forEach((tab, index) => {
-                tab.isActive = (index === ID)
-            })
-        }
-    },
-    computed: {
-        radiusStyle(){
-            let i,radiusStyle;
-            for(i=0; i<this.radiusSizes.length;i++){
-                if(i === this.radius-1){
-                    radiusStyle = this.radiusSizes[i];
-                }
-            }
-            return radiusStyle
-        }
+  name: 'TTab',
+  props: {
+    active: {
+      type: Number,
+      default: 1
     }
+  },
+  mixins: [tabStyleMixin],
+  data() {
+    return {
+      activeID: null,
+      tabs: [],
+      isActive: false,
+    }
+  },
+  created() {
+    this.tabs = this.$children
+  },
+  mounted() {
+    this.selectedTab(this.active - 1)
+  },
+  methods: {
+    selectedTab(ID) {
+      this.activeID = ID;
+      this.tabs.forEach((tab, index) => {
+        tab.isActive = (index === ID)
+      })
+    }
+  },
 }
 </script>
+
