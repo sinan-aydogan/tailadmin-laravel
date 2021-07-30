c48d2470467d82a08768fc76df5fc3cd22c3ea1e
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-07-30 03:37:46 +0300

    Toastr: Style Improvements (Purge Problem Solved) -Simple colors constants changed: red -> solid-red -Gradient colors added -Gradient's Direction of the bg color added

--- resources/js/Components/Toastr/TToastr.vue
+++ resources/js/Components/Toastr/TToastr.vue
@@ -1,14 +1,17 @@
 <template>
   <transition name="fade">
     <div v-if="showAlertBox"
-         :class="[alertBoxStyle,toastrStyle,radiusStyle,border && 'border', position ? positions[position].positionStyle : 'relative w-full overflow-hidden flex-shrink-0']">
+         :class="[
+             'toastr',
+             calculatedToastrStyle,
+             ]">
       <div class="flex px-1 items-center justify-start gap-2">
         <slot name="icon"></slot>
         <div class="flex flex-wrap items-center whitespace-normal flex-shrink-0">
           <slot></slot>
         </div>
       </div>
-      <div v-if="closeable" :class="alertBoxCloseIconStyle" @click="close">
+      <div v-if="closeable" :class="['toastr-close', closeIconColors[color]]" @click="close">
         <t-x-circle-icon class="w-5 h-5 text-white"/>
       </div>
     </div>
@@ -16,24 +19,16 @@
 </template>
 
 <script>
-import {radiusSizeMixin} from "@/Mixins/radiusSizeMixin";
+import {toastrStyleMixin} from "@/Mixins/Styles/toastrStyleMixin";
 import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
 
 export default {
   name: "TToastr",
   components: {TXCircleIcon},
-  mixins: [radiusSizeMixin],
+  mixins: [toastrStyleMixin],
   props: {
     id: {
       type: String
-    },
-    color: {
-      required: false,
-      default: 'red'
-    },
-    border: {
-      type: Boolean,
-      default: false
     },
     closeable: {
       type: Boolean,
@@ -44,33 +39,10 @@ export default {
       type: Number,
       required: false
     },
-    position: {
-      type: String,
-      required: false
-    }
   },
   data() {
     return {
       showAlertBox: true,
-      positions: {
-        'lb': {
-          positionStyle: 'fixed left-4 bottom-4'
-        },
-        'rb': {
-          positionStyle: 'fixed right-6 bottom-4'
-        },
-        'lt': {
-          positionStyle: 'fixed left-4 top-4'
-        },
-        'rt': {
-          positionStyle: 'fixed right-6 top-4'
-        },
-        '': {
-          positionStyle: 'relative w-full'
-        }
-      },
-      alertBoxStyle: 'flex flex-row justify-between items-center shadow p-4 min-h-max',
-      alertBoxCloseIconStyle: 'flex items-center justify-center w-5 h-5 rounded-full text-' + this.color + '-500 font-bold border-' + this.color + '-500 hover:bg-red-700 hover:text-white hover:bg-opacity-90 cursor-pointer'
     }
   },
   created() {
@@ -87,27 +59,6 @@ export default {
       this.$emit('destroy', this.id)
     }
   },
-  computed: {
-    toastrStyle() {
-      /*Color Styles*/
-      /*Solid*/
-      if (!this.color.includes('-') && this.color !== 'black' && this.color !== 'white') {
-        return 'bg-' + this.color + '-500 text-white';
-      } else if (this.color === 'black') {
-        return 'bg-black text-white'
-      } else if (this.color === 'white') {
-        return 'bg-white border border-gray-300 text-gray-700'
-      }
-      /*Light*/
-      if (this.color.includes('light')) {
-        return 'bg-' + this.color.split('-')[1] + '-50 border border-' + this.color.split('-')[1] + '-500 text-' + this.color.split('-')[1] + '-600';
-      }
-      /*Gradient*/
-      if (this.color.includes('gradient')) {
-        return 'bg-gradient-to-r from-' + this.color.split('-')[1] + '-500 to-' + this.color.split('-')[3] + '-700 text-white';
-      }
-    }
-  }
 }
 </script>
 
