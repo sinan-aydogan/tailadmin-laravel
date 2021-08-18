0dca9b18cacfe5f4b0b0682339faac0b5de27193
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-08-18 15:56:34 +0300

    Select Input working method changed #Added -"options" for data set -"optionsLabelKey" for label visibibility -"optionsValueKey" for data select and set -Scoped Slot logic for rich options
    #Deceprated
    -Select Input Item component
    
    You should link options data set with Select Input option attribute.

--- resources/js/Components/Form/Inputs/TInputSelect.vue
+++ resources/js/Components/Form/Inputs/TInputSelect.vue
@@ -1,59 +1,101 @@
 <template>
   <div v-click-outside="outside" class="relative select-none min-w-3">
-    <div :class="['form-input hover:border-gray-400 bg-white h-10 flex items-center cursor-pointer',radiusStyle]">
-      <slot></slot>
-      <!--Placeholder Text-->
-      <span v-if="selectedID === null" :class="['w-full p-2',disabled && 'text-gray-300']"
-            aria-disabled="true"
-            @click="changeShowOptions">
-                {{ placeHolder ? placeHolder : 'Select' }}
-            </span>
-      <!--Selected Option-->
-      <div v-if="selectedID !== null" class="p-2 w-full" @click="changeShowOptions">
-                <span v-if="selectedOption.componentInstance.$el.innerHTML"
-                      v-html="selectedOption.componentInstance.$el.innerHTML"/>
-        <span v-else v-html="selectedOption.componentInstance.label"/>
+    <!--Container-->
+    <div
+        :class="['select-container',radiusStyle]"
+        @click="changeShowOptions"
+    >
+
+      <!--Texts-->
+      <div class="inline-flex w-full">
+
+        <!--Placeholder Text-->
+        <span
+            v-if="value === null"
+            v-text="placeHolder ? placeHolder : 'Select'"
+        />
+
+        <!--Selected Option-->
+        <span v-else>
+          <!--ScopeSlot Rich Label-->
+          <slot
+              v-if="$scopedSlots.label"
+              :props="selectedOption"
+              name="label"
+              v-html="selectedOption[optionsLabelKey]"
+          />
+          <!--Simple Text Label-->
+        <span
+            v-if="!$scopedSlots.label"
+            v-text="selectedOption[optionsLabelKey]"
+        />
+        </span>
+
       </div>
-      <!--Clear Button-->
-      <div v-if="clearButton && selectedID" @click="selectedID = null;searchText = '';$emit('input',null)">
-        <t-x-circle-icon class="w-6 h-6 rounded-full hover:bg-red-500 hover:text-white cursor-pointer"/>
-      </div>
-      <!--DropDown Icon-->
-      <div @click="changeShowOptions">
+
+      <!--Icons-->
+      <div class="inline-flex">
+
+        <!--Clear Button-->
+        <div
+            v-if="clearButton && selectedOption"
+            @click="selectedOption = null;searchText = '';$emit('input',null)"
+        >
+          <t-x-circle-icon class="w-6 h-6 rounded-full hover:bg-red-500 hover:text-white cursor-pointer"/>
+        </div>
+
+        <!--Dropdown Icon-->
         <t-chevron-down-icon
             :class="[
-                'w-6 h-6 transform duration-300',
+                'select-dropdown-icon',
                 showOptions ? 'rotate-90' : 'rotate-0'
                 ]"
+            @click="changeShowOptions"
         />
       </div>
-      <!--Options Container-->
-      <div v-if="showOptions"
-           class="absolute flex flex-col top-11 bg-white border border-gray-300 rounded-md -ml-2 shadow-lg z-50">
-        <!--Search Box-->
-        <div v-if="search" class="flex w-full p-2 bg-gray-200">
-          <t-input-text v-model="searchText"
-                        :placeholder="searchPlaceHolder ? searchPlaceHolder : 'Search...'"></t-input-text>
-        </div>
-        <!--Options List-->
-        <div
-            v-for="(item,index) in searchList"
-            v-if="index<10"
-            :key="index"
-            :class="[
-                'flex hover:bg-gray-200 p-2 cursor-pointer whitespace-nowrap',
-                alignStyle[align]
-            ]"
-            @click="
-            !item.componentInstance.$attrs.disabled && $emit('input', item.componentInstance.value);
-            !item.componentInstance.$attrs.disabled && (showOptions = !showOptions);
-            !item.componentInstance.$attrs.disabled && select(index)">
-          <div v-if="item.componentInstance.$el.innerHTML" v-html="item.componentInstance.$el.innerHTML"/>
-          <div v-else v-html="item.componentInstance.label"/>
-        </div>
-        <div v-if="searchList.length>10" class="hidden tablet:block text-xs text-center p-2 text-blue-500">The results({{searchList.length}}) are
-          too many, please search...
-        </div>
+    </div>
+
+    <!--Options Container-->
+    <div v-if="showOptions" class="select-options-container">
+      <!--Search Box-->
+      <div v-if="search" class="flex w-full p-2 bg-gray-200">
+        <t-input-text
+            v-model="searchText"
+            :placeholder="searchPlaceHolder ? searchPlaceHolder : 'Search...'"
+        />
+      </div>
+      <!--Options List-->
+      <div
+          v-for="(item,index) in searchedList"
+          v-if="index<10"
+          :key="item[optionsValueKey]"
+          :class="[
+              'select-option-item',
+              alignStyle[align]
+              ]"
+          @click="select(item)"
+      >
+        <!--ScopeSlot Rich Label-->
+        <slot
+            v-if="$scopedSlots.label"
+            :props="item"
+            name="label"
+            v-html="item[optionsLabelKey]"
+        />
+        <!--Simple Text Label-->
+        <span
+            v-else
+            v-text="item[optionsLabelKey]"
+        />
+      </div>
+      <!--Many Items Notification-->
+      <div
+          v-if="searchedList.length>10"
+          class="hidden tablet:block text-xs text-center p-2 text-blue-500"
+      >
+        The
+        results({{ searchedList.length }}) are
+        too many, please search...
       </div>
     </div>
   </div>
@@ -69,7 +111,7 @@ import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
 export default {
   name: "TInputSelect",
   components: {TXCircleIcon, TChevronLeftIcon, TChevronDownIcon, TInputText},
-  props: ['value', 'placeHolder', 'searchPlaceHolder', 'clearButton', 'disabled', 'search', 'align'],
+  props: ['value', 'options', 'optionsLabelKey', 'optionsValueKey', 'placeHolder', 'searchPlaceHolder', 'clearButton', 'disabled', 'search', 'align'],
   mixins: [radiusSizeMixin],
   directives: {
     'click-outside': {
@@ -107,11 +149,9 @@ export default {
   },
   data() {
     return {
+      selectedOption: null,
+      searchedList: this.options,
       searchText: '',
-      searchList: [],
-      options: [],
-      selectedID: null,
-      selectedOption: {},
       showOptions: false,
       alignStyle: {
         'left': 'text-left',
@@ -120,15 +160,18 @@ export default {
       }
     }
   },
+  created() {
+    if (this.value !== null) {
+      this.options.forEach((item) => {
+        if (this.value === item[this.optionsValueKey]) {
+          this.selectedOption = item
+        }
+      })
+    }
+  },
   methods: {
     outside() {
       this.showOptions = false
-    },
-    select(index) {
-      let options = [...this.options];
-      this.selectedID = options[index].componentInstance._uid;
-      let selected = options.filter(item => item.componentInstance._uid === this.selectedID)
-      this.selectedOption = selected[0];
     },
     changeShowOptions() {
       if (this.disabled) {
@@ -137,26 +180,18 @@ export default {
         this.showOptions = !this.showOptions
       }
     },
-  },
-  created() {
-    this.options = this.$slots.default;
-    if (this.searchText === '') {
-      let list;
-      list = this.options;
-      list.forEach(item => {
-        if (item.tag !== undefined) {
-          this.searchList = [...this.searchList, item]
-        }
-      })
-      this.options = this.searchList
+    select(item) {
+      this.selectedOption = item;
+      this.$emit('input', item[this.optionsValueKey])
+      this.showOptions = false
     }
   },
   watch: {
     searchText() {
       if (this.searchText !== '') {
-        this.searchList = this.options.filter(option => option.componentInstance.$el.innerHTML.toLowerCase().replace(/[^a-zA-Z ]/g, "").indexOf(this.searchText.toLowerCase().replace(/[^a-zA-Z ]/g, "")) > -1)
+        this.searchedList = this.options.filter(option => option[this.optionsLabelKey].toLowerCase().replace(/[^a-zA-Z ]/g, "").indexOf(this.searchText.toLowerCase().replace(/[^a-zA-Z ]/g, "")) > -1)
       } else {
-        this.searchList = this.options
+        this.searchedList = this.options
       }
     }
   }
--- resources/js/Pages/Samples/FormElements/SelectInput.vue
+++ resources/js/Pages/Samples/FormElements/SelectInput.vue
@@ -20,24 +20,35 @@
             title="Personal Infos">
           <!-- Manager -->
           <t-input-group class="col-span-12 md:col-span-6" label="Manager - Searchable" labelFor="user_id">
-            <t-input-select v-model="form.user_id" :clear-button="true" :search="true" place-holder="Select"
-                            search-place-holder="Search...">
-              <t-input-select-item v-for="(item,index) in users" :key="index" :value="item.id"><span
-                  :class="item.class">{{ item.name }}</span></t-input-select-item>
-            </t-input-select>
+            <t-input-select
+                v-model="form.user_id"
+                :clear-button="true"
+                :options="users"
+                :search="true"
+                optionsLabelKey="name"
+                optionsValueKey="id"
+                place-holder="Select a manager"
+                search-place-holder="Search a manager..."
+            />
           </t-input-group>
           <!-- Status -->
           <t-input-group class="col-span-12 md:col-span-6" label="Status" labelFor="status">
-            <t-input-select :clear-button="true" place-holder="Select"
-                            search-place-holder="Search...">
-              <t-input-select-item value="1">
-                <font-awesome-icon class="text-green-500 mr-2" icon="check"/>
-                Active
-              </t-input-select-item>
-              <t-input-select-item value="2">
-                <font-awesome-icon class="text-red-500 mr-2" icon="minus-circle"/>
-                Passive
-              </t-input-select-item>
+            <t-input-select
+                v-model="form.status"
+                :clear-button="true"
+                :options="status"
+                optionsLabelKey="name"
+                optionsValueKey="value"
+                place-holder="Select a status"
+            >
+              <template #label="{ props }">
+                <span v-if="props.value===1" class="flex flex-row items-center gap-1">
+                  <t-check-circle-icon  class="w-5 h-5 text-green-500 "/> {{ props.name }}
+                </span>
+                <span v-if="props.value===0" class="flex flex-row items-center gap-1">
+                  <t-x-circle-icon class="w-5 h-5 text-red-500 items-center"/> {{ props.name }}
+                </span>
+              </template>
             </t-input-select>
           </t-input-group>
         </t-form-section>
@@ -69,14 +80,16 @@ import SshPre from 'simple-syntax-highlighter'
 import 'simple-syntax-highlighter/dist/sshpre.css'
 import TInputGroup from "@/Components/Form/TInputGroup";
 import TInputSelect from "@/Components/Form/Inputs/TInputSelect";
-import TInputSelectItem from "@/Components/Form/Inputs/TInputSelectItem";
 import TTable from "@/Components/Table/TTable";
+import TCheckCircleIcon from "@/Components/Icon/TCheckCircleIcon";
+import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
 
 export default {
   name: "SelectInput",
   components: {
+    TXCircleIcon,
+    TCheckCircleIcon,
     TTable,
-    TInputSelectItem,
     TInputSelect,
     AppLayout,
     TButton,
@@ -91,13 +104,8 @@ export default {
       loading: false,
       form: this.$inertia.form({
         _method: 'POST',
-        name: null,
         user_id: null,
-        tax_id: null,
-        email: null,
-        phone: null,
-        status: null,
-        address: null,
+        status: 1,
       }),
       status: [
         {name: 'Passive', value: 0, icon: 'XIcon', class: 'w-5 h-5 text-red-500 mr-2'},
@@ -107,6 +115,9 @@ export default {
         html:
             '<t-input-group label="Manager - Searchable" labelFor="user_id">\n' +
             '    <t-input-select\n' +
+            '        :options="users"\n' +
+            '        optionsLabelKey="name"\n' +
+            '        optionsValueKey="id"\n' +
             '        v-model="form.user_id"\n' +
             '        :clear-button="true"\n' +
             '        :search="true"\n' +
@@ -116,31 +127,34 @@ export default {
             '        place-holder="Select"\n' +
             '        search-place-holder="Search..."\n' +
             '    >\n' +
-            '        <!--If you work with static values, you can clone this component-->\n' +
-            '        <t-input-select-item\n' +
-            '            v-for="(item,index) in users"\n' +
-            '            :key="index"\n' +
-            '            :value="item.id"\n' +
-            '        >\n' +
-            '            <!--You are free here, you can use to image, bold, colorful, flag here-->\n' +
-            '            <span>\n' +
-            '                {{ item.name }}\n' +
+            '        <!--If you want to rich styled options, you can use to this template. You can use any data key after props attribute between the template tags.-->\n' +
+            '        <template #label="{ props }">\n' +
+            '            <span v-if="props.value===1" class="flex flex-row items-center gap-1">\n' +
+            '              <t-check-circle-icon  class="w-5 h-5 text-green-500 "/> {{ props.name }}\n' +
             '            </span>\n' +
-            '        </t-input-select-item>\n' +
+            '            <span v-if="props.value===0" class="flex flex-row items-center gap-1">\n' +
+            '              <t-x-circle-icon class="w-5 h-5 text-red-500 items-center"/> {{ props.name }}\n' +
+            '            </span>\n' +
+            '        </template>\n' +
             '    </t-input-select>\n' +
             '</t-input-group>',
         js: 'import TInputGroup from "@/Components/Form/TInputGroup";\n' +
             'import TInputSelect from "@/Components/Form/Inputs/TInputSelect";\n' +
-            'import TInputSelectItem from "@/Components/Form/Inputs/TInputSelectItem";\n\n' +
             'export default {\n' +
             '    name: "FormStructure",\n' +
-            '    components: {TInputGroup, TInputSelect, TInputSelectItem},\n' +
+            '    components: {TInputGroup, TInputSelect},\n' +
             '    data() {\n' +
             '        return {\n' +
             '            loading: false,\n' +
+            '            /*Select Options List*/\n' +
+            '            status: [\n' +
+            '                    {name: \'Passive\', value: 0, icon: \'XIcon\', class: \'w-5 h-5 text-red-500 mr-2\'},\n' +
+            '                    {name: \'Active\', value: 1, icon: \'Checked\', class: \'w-5 h-5 text-green-500 mr-2\'}\n' +
+            '                  ],\n' +
             '            form: this.$inertia.form({\n' +
             '                _method: \'POST\',\n' +
             '                user_id : null,\n' +
+            '                status : null, /*You can set the default value here*/\n' +
             '            }),\n' +
             '        };\n' +
             '    },\n' +
@@ -173,6 +187,21 @@ export default {
             }
           ],
           content: [
+            {
+              variable: ":options",
+              type: "Object or Array",
+              details: "Your select options data must be defined like a object or an array"
+            },
+            {
+              variable: "optionsLabelKey",
+              type: "String",
+              details: "This is the showing label your users for selecting, You should enter a key from your options data."
+            },
+            {
+              variable: "optionsValueKey",
+              type: "String",
+              details: "This is the value key of the option for the unique selection. You should enter a key from your options data, like: id, value"
+            },
             {
               variable: ":clear-button",
               type: "Boolean",
