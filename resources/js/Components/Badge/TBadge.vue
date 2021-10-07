b1f5550f1a9cae8f452e00dd225df3a3413f9426
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-10-07 05:31:57 +0300

    Badge: Component integrated Vue3

--- resources/js/Components/Badge/TBadge.vue
+++ resources/js/Components/Badge/TBadge.vue
@@ -1,94 +1,191 @@
 <template>
-  <div
-      :class="[
-            'badge',
-            collapsible && 'cursor-pointer',
-            calculatedBadgeStyle
-        ]"
-      @click="toggle"
-  >
-    <!--Badge Icon-->
     <div
-        v-if="$slots.icon"
         :class="[
-            'badge-icon',
-            collapsible ? 'mx-0.5' : 'ml-0.5'
-            ]"
+            'badge',
+            (collapsible || (selectable && !selectIndicator)) && 'cursor-pointer',
+            selectable && !modelValue && !selectActiveStyle && 'filter grayscale opacity-50',
+            radiusStyle,
+            styleClass()
+        ]"
+        @click.stop="toggle(); (selectable && !selectIndicator) && select()"
     >
-      <slot name="icon" @click="toggle"></slot>
+        <!--Inline Line-->
+        <div v-if="design.includes('line')"
+             id="line"
+             :class="[design === 'inline' ? 'badge-inline-line' : 'badge-outline-line']"
+        />
+
+        <!--Badge Icon-->
+        <div
+            v-if="$slots['icon'] && design !== 'outline'"
+            :class="[
+            'badge-icon',
+            5 > radius && design  !== 'block' && !collapsible && 'ml-0.5',
+            5 <= radius && design === 'inline' && 'ml-0.5'
+            ]"
+        >
+            <slot name="icon" @click="toggle"></slot>
+        </div>
+
+        <!--Badge Content-->
+        <transition name="show-content">
+            <div v-if="showContent || !collapsible" class="badge-content-container" :key="badge-content">
+                <!--Content-->
+                <div :class="[
+                    'badge-content',
+                    $slots['icon'] &&  '-ml-0.5'
+                    ]">
+                    <slot/>
+                </div>
+                <!--Badge Value-->
+                <div v-if="badgeValue"
+                     :class="[
+                         'badge-value',
+                         radius > 4  && design !== 'elegant' && 'mr-0.5',
+                         badgeValue.length===1 ? 'px-1.5' : 'px-1'
+                         ]"
+                >
+                    {{ badgeValue }}
+                </div>
+                <!--Selected Icon-->
+                <div v-if="selectable">
+                    <!--Custom Selected Icon-->
+                    <div v-if="$slots['selected-icon']" key="selected-icon" class="badge-select-icon" @click="select">
+                        <slot name="selected-icon"></slot>
+                    </div>
+                    <!--Custom Unselected Icon-->
+                    <div v-if="$slots['unselected-icon']" key="unselected-icon" class="badge-select-icon" @click="select">
+                        <slot name="unselected-icon"></slot>
+                    </div>
+                </div>
+                <!--Select Indicator-->
+                <div
+                    v-if="selectable && selectIndicator"
+                    class="badge-select-icon"
+                    @click="select"
+                >
+                    <t-x-circle-icon v-if="modelValue" class="w-4 h-4"/>
+                    <t-plus-icon v-else class="w-4 h-4"/>
+                </div>
+            </div>
+        </transition>
     </div>
-    <!--Badge Content-->
-    <transition name="fade">
-      <div
-          v-if="showTitle || !collapsible"
-          :class="[
-              'badge-container',
-              $slots.icon ? 'pr-1.5' : 'px-2'
-              ]">
-        <!--Content-->
-        <div :class="[
-            'badge-content',
-            $slots.icon && !collapsible && 'ml-0.5',
-            ]">
-          <slot></slot>
-        </div>
-        <!--Value-->
-        <div v-if="value" :class="[
-            'badge-value',
-            $slots.icon ? '-mr-0.5' : '-mr-1.5'
-            ]">
-          {{ value }}
-        </div>
-      </div>
-    </transition>
-  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import {badgeStyleMixin} from "@/Mixins/Styles/badgeStyleMixin";
+import {defineComponent, ref, toRefs} from 'vue'
+import {radiusSizeMixin} from "@/Mixins/radiusSizeMixin";
+import TPlusIcon from "@/Components/Icon/TPlusIcon";
+import TIconMinus from "@/Components/Icon/solid/TIconMinus";
+import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
 
 export default defineComponent({
-  name: "TBadge",
-  mixins: [badgeStyleMixin],
-  props: {
-    value: {
-      type: String,
-      required: false
+    name: "TBadge",
+    components: {TXCircleIcon, TIconMinus, TPlusIcon},
+    mixins: [radiusSizeMixin],
+    props: {
+        design: {
+            type: String,
+            default: 'filled'
+        },
+        color: {
+            type: String,
+            default: 'white'
+        },
+        selectable: {
+            type: Boolean,
+            default: false
+        },
+        selectIndicator: {
+            type: Boolean,
+            default: false
+        },
+        selectActiveStyle: {
+            type: Object,
+        },
+        selectPassiveStyle: {
+            type: Object,
+        },
+        modelValue: {
+            type: Boolean,
+            default: false
+        },
+        badgeValue: {
+            type: [Number, String],
+            required: false
+        },
+        collapsible: {
+            type: Boolean,
+            default: false
+        }
     },
-    collapsible: {
-      type: Boolean,
-      default: false
+    setup(props, {emit}) {
+        const {design, color, selectable, selectActiveStyle, selectPassiveStyle} = toRefs(props)
+
+        /*Select*/
+        const value = ref(props.modelValue)
+        const select = () => {
+            value.value = !value.value
+            emit('update:modelValue', value.value)
+        }
+
+        /*Toggle*/
+        const showContent = ref(false)
+        const toggle = () => {
+            showContent.value = !showContent.value
+        }
+
+        /*Design Check*/
+        const styleClass = () => {
+            const activeColor = ref();
+            const activeDesign = ref();
+
+            if(selectable.value){
+                /*Select Style Check*/
+                if(selectActiveStyle.value && !selectPassiveStyle.value){
+                    selectPassiveStyle.value = selectActiveStyle.value
+                }
+                if(value.value && selectActiveStyle.value){
+                    activeColor.value = selectActiveStyle.value['color'];
+                    activeDesign.value = selectActiveStyle.value['design'];
+                }else if(!value.value && selectActiveStyle.value){
+                    activeColor.value = selectPassiveStyle.value['color'];
+                    activeDesign.value = selectPassiveStyle.value['design'];
+                }else{
+                    activeColor.value = color.value;
+                    activeDesign.value = design.value;
+                }
+            } else {
+                activeColor.value = color.value;
+                activeDesign.value = design.value;
+            }
+
+            console.log(activeDesign.value)
+            return 'badge-' + activeDesign.value + '-' + activeColor.value + ' badge-' + activeDesign.value + '-base'
+        }
+
+        const hasSlot = name => !!slots[name]
+
+        return {hasSlot, showContent, toggle, select, styleClass}
     }
-  },
-  data() {
-    return {
-      showTitle: false,
-    };
-  },
-  methods: {
-    toggle() {
-      this.showTitle = !this.showTitle;
-    }
-  }
 });
 </script>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s;
+.show-content-enter-active,
+.show-content-leave-active {
+    transition: all ease-in-out 1s;
 }
 
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-  max-width: 0;
+.show-content-enter-from,
+.show-content-leave-to {
+    opacity: 0;
+    max-width: 0;
 }
 
-.fade-enter-to,
-.fade-leave {
-  opacity: 1;
-  max-width: 250px;
+.show-content-enter-to,
+.show-content-leave-from {
+    opacity: 1;
+    max-width: 250px;
 }
 </style>
