f21ea3cd607ecd6e3ba026b2bc0f69fbd584d59d
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-11-09 18:29:11 +0300

    Dark mode base added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    darkMode: 'class',
 
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
