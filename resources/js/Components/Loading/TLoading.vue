90e041b955d958a980e5c685260ef8e728409044
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-11-18 22:46:30 +0300

    Loading showing method changed from v-if to v-model

--- resources/js/Components/Loading/TLoading.vue
+++ resources/js/Components/Loading/TLoading.vue
@@ -1,41 +1,43 @@
 <template>
-  <div :class="tStyle">
-    <!--Countdown Line-->
-    <div v-if="timer" class="loading-screen-countdown">
-      <div id="countdown" :style="{width: countDownCounter+'%'}"></div>
-    </div>
-    <!--Close Icon-->
-    <div v-if="closeable" class="loading-screen-close" @click="close">
-
-      <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-      </svg>
-    </div>
-    <!--Content-->
-    <div v-if="!hasSlot('default')" class="loading-screen-content">
-      <!--Text Content-->
-      <div class="flex flex-col h-full space-y-8 justify-center items-center">
-        <!--Title-->
-        <span id="title">
+  <transition name="fade">
+    <div :class="tStyle" v-if="modelValue">
+      <!--Countdown Line-->
+      <div v-if="timer" class="loading-screen-countdown">
+        <div id="countdown" :style="{width: countDownCounter+'%'}"></div>
+      </div>
+      <!--Close Icon-->
+      <div v-if="closeable" class="loading-screen-close" @click="close">
+        <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </div>
+      <!--Content-->
+      <div v-if="!hasSlot('default')" class="loading-screen-content">
+        <!--Text Content-->
+        <div id="wrapper">
+          <!--Title-->
+          <span id="title">
           {{ title }}
         </span>
-        <!--Message-->
-        <span id="message">
+          <!--Message-->
+          <span id="message">
           {{ message }}
         </span>
+        </div>
+        <!--Animation-->
+        <component
+          id="animation"
+          :is="loadingComponent"
+          :color="loadingColor ? loadingColor : color"
+          target="loading-screen"
+        />
       </div>
-      <!--Animation-->
-      <component id="animation"
-                 :is="loadingComponent"
-                 :color="loadingColor ? loadingColor : color"
-                 target="loading-screen"
-      />
+      <!--Custom Content-->
+      <div v-if="hasSlot('default')" id="custom-content">
+        <slot></slot>
+      </div>
     </div>
-    <!--Custom Content-->
-    <div v-if="hasSlot('default')">
-      <slot></slot>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -44,6 +46,10 @@ import { computed, defineAsyncComponent, defineComponent, ref, toRefs } from "vu
 export default defineComponent({
   name: "TLoading",
   props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    },
     color: {
       type: String,
       default: "gray"
@@ -74,7 +80,7 @@ export default defineComponent({
       required: false
     }
   },
-  emits: ["close"],
+  emits: ["update:modelValue"],
   setup(props, { emit, slots }) {
     const { timer, color, loadingDesign } = toRefs(props);
 
@@ -107,7 +113,7 @@ export default defineComponent({
 
     /*Close*/
     const close = () => {
-      emit("close", false);
+      emit("update:modelValue", false);
     };
 
     /*Loading Component*/
@@ -129,3 +135,22 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+/*Show Hide Menu Titles*/
+.fade-enter-active, .fade-leave-active {
+  transition: all 700ms ease-in-out;
+}
+
+.fade-enter-from, .fade-leave-to {
+  transform: scaleX(1.1);
+  filter: blur(4px);
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+  transform: scaleX(1);
+  filter: blur(0);
+  opacity: 1;
+}
+</style>
