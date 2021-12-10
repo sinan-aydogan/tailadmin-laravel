a4660ea332ec44ed90a4da791bb85fa5b6155006
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-12-10 17:20:24 +0300

    disabled and readonly samples added

--- resources/js/Pages/Samples/FormElements/SelectInput.vue
+++ resources/js/Pages/Samples/FormElements/SelectInput.vue
@@ -1,60 +1,83 @@
 <template>
-  <app-layout :actionButtons="true">
-    <template #header>
-      Select Input Component
-    </template>
-    <template #action-buttons>
-      <t-button :radius="3" color="yellow">
-        <font-awesome-icon icon="angle-left"/>
-        Back to Home
-      </t-button>
-      <t-button :radius="3" color="green">
-        <font-awesome-icon icon="plus-circle"/>
-        Add New
-      </t-button>
-    </template>
-    <template #default>
-      <t-form-content @reset="reset" @submitted="save">
-        <t-form-section
-            description="This information is subject to personal data protection law."
-            title="Personal Infos">
-          <!-- Manager -->
-          <t-input-group class="col-span-12 md:col-span-6" label="Manager - Searchable" labelFor="user_id">
-            <t-input-select
-                v-model="form.user_id"
-                :clear-button="true"
-                :options="users"
-                :search="true"
-                optionsLabelKey="name"
-                optionsValueKey="id"
-                place-holder="Select a manager"
-                search-place-holder="Search a manager..."
-            />
-          </t-input-group>
-          <!-- Status -->
-          <t-input-group class="col-span-12 md:col-span-6" label="Status" labelFor="status">
-            <t-input-select
-                v-model="form.status"
-                :clear-button="true"
-                :options="status"
-                optionsLabelKey="name"
-                optionsValueKey="value"
-                place-holder="Select a status"
-            >
-              <template #label="{ props }">
+    <app-layout :actionButtons="true">
+        <template #header>
+            Select Input Component
+        </template>
+        <template #action-buttons>
+            <t-button :radius="3" color="yellow">
+                <font-awesome-icon icon="angle-left"/>
+                Back to Home
+            </t-button>
+            <t-button :radius="3" color="green">
+                <font-awesome-icon icon="plus-circle"/>
+                Add New
+            </t-button>
+        </template>
+        <template #default>
+            <t-form-content @reset="reset" @submitted="save">
+                <t-form-section
+                    description="This information is subject to personal data protection law."
+                    title="Personal Infos">
+                    <!-- Manager -->
+                    <t-input-group class="col-span-12 md:col-span-6" label="Manager - Searchable" labelFor="user_id">
+                        <t-input-select
+                            v-model="form.user_id"
+                            :clear-button="true"
+                            :options="users"
+                            :search="true"
+                            optionsLabelKey="name"
+                            optionsValueKey="id"
+                            place-holder="Select a manager"
+                            search-place-holder="Search a manager..."
+                        />
+                    </t-input-group>
+                    <!-- Rich Options Content -->
+                    <t-input-group class="col-span-12 md:col-span-6" label="Rich Options Content" labelFor="status">
+                        <t-input-select
+                            v-model="form.status"
+                            :clear-button="true"
+                            :options="status"
+                            optionsLabelKey="name"
+                            optionsValueKey="value"
+                            place-holder="Select a status"
+                        >
+                            <template #label="{ props }">
                 <span v-if="props.value===true" class="flex flex-row items-center gap-1">
-                  <t-check-circle-icon  class="w-5 h-5 text-green-500 "/> {{ props.name }}
+                  <t-check-circle-icon class="w-5 h-5 text-green-500 "/> {{ props.name }}
                 </span>
-                <span v-if="props.value===false" class="flex flex-row items-center gap-1">
+                                <span v-if="props.value===false" class="flex flex-row items-center gap-1">
                   <t-x-circle-icon class="w-5 h-5 text-red-500 items-center"/> {{ props.name }}
                 </span>
-              </template>
-            </t-input-select>
-          </t-input-group>
-        </t-form-section>
-      </t-form-content>
-    </template>
-  </app-layout>
+                            </template>
+                        </t-input-select>
+                    </t-input-group>
+                    <!--Disabled-->
+                    <t-input-group label="Disabled" class="col-span-12 md:col-span-6">
+                        <t-input-select disabled/>
+                    </t-input-group>
+                    <!--Read Only-->
+                    <t-input-group label="Read Only" class="col-span-12 md:col-span-6">
+                        <t-input-select
+                            :model-value="true"
+                            :options="status"
+                            options-value-key="value"
+                            options-label-key="name"
+                            read-only
+                        >
+                            <template #label="{ props }">
+                                <span v-if="props.value===true" class="flex flex-row items-center gap-1">
+                                    <t-check-circle-icon class="w-5 h-5 text-green-500 "/> {{ props.name }}
+                                </span>
+                                <span v-if="props.value===false" class="flex flex-row items-center gap-1">
+                                    <t-x-circle-icon class="w-5 h-5 text-red-500 items-center"/> {{ props.name }}
+                                </span>
+                            </template>
+                        </t-input-select>
+                    </t-input-group>
+                </t-form-section>
+            </t-form-content>
+        </template>
+    </app-layout>
 </template>
 
 <script>
@@ -69,50 +92,50 @@ import TCheckCircleIcon from "@/Components/Icon/TCheckCircleIcon";
 import TXCircleIcon from "@/Components/Icon/TXCircleIcon";
 
 export default {
-  name: "SelectInput",
-  components: {
-    TXCircleIcon,
-    TCheckCircleIcon,
-    TInputSelect,
-    AppLayout,
-    TButton,
-    TFormSection,
-    TFormContent,
-    TInputGroup,
-  },
-  props: ['users'],
-  data() {
-    return {
-      loading: false,
-      form: this.$inertia.form({
-        _method: 'POST',
-        user_id: null,
-        status: false
-      }),
-      status: [
-        {name: 'Passive', value: false, icon: 'XIcon', class: 'w-5 h-5 text-red-500 mr-2'},
-        {name: 'Active', value: true, icon: 'Checked', class: 'w-5 h-5 text-green-500 mr-2'}
-      ]
-    };
-  },
-  methods: {
-    reset: function () {
-      this.form.name = null;
-      this.form.user_id = null;
-      this.form.tax_id = null;
-      this.form.email = null;
-      this.form.phone = null;
-      this.form.status = null;
-      this.form.address = null;
+    name: "SelectInput",
+    components: {
+        TXCircleIcon,
+        TCheckCircleIcon,
+        TInputSelect,
+        AppLayout,
+        TButton,
+        TFormSection,
+        TFormContent,
+        TInputGroup,
     },
-    save() {
-      this.form.post(route('customer.store'), {
-        errorBag: 'customer',
-        preserveScroll: true,
-      });
-      this.loading = true;
-    }
-  },
+    props: ['users'],
+    data() {
+        return {
+            loading: false,
+            form: this.$inertia.form({
+                _method: 'POST',
+                user_id: null,
+                status: false
+            }),
+            status: [
+                {name: 'Passive', value: false, icon: 'XIcon', class: 'w-5 h-5 text-red-500 mr-2'},
+                {name: 'Active', value: true, icon: 'Checked', class: 'w-5 h-5 text-green-500 mr-2'}
+            ]
+        };
+    },
+    methods: {
+        reset: function () {
+            this.form.name = null;
+            this.form.user_id = null;
+            this.form.tax_id = null;
+            this.form.email = null;
+            this.form.phone = null;
+            this.form.status = null;
+            this.form.address = null;
+        },
+        save() {
+            this.form.post(route('customer.store'), {
+                errorBag: 'customer',
+                preserveScroll: true,
+            });
+            this.loading = true;
+        }
+    },
 }
 </script>
 
