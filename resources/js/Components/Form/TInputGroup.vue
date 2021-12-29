76ff8e55bc8749e564ba977910d78d142e3e5559
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-12-29 12:23:42 +0300

    error length error fixes

--- resources/js/Components/Form/TInputGroup.vue
+++ resources/js/Components/Form/TInputGroup.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="flex flex-col">
-    <!--Label-->
-    <label
-        :for="labelFor"
-        class="flex flex-row gap-1 items-center font-medium text-sm whitespace-normal dark:text-gray-200"
-    >
-      <!--Label-->
-      <span v-if="label">
+    <div class="flex flex-col">
+        <!--Label-->
+        <label
+            :for="labelFor"
+            class="flex flex-row gap-1 items-center font-medium text-sm whitespace-normal dark:text-gray-200"
+        >
+            <!--Label-->
+            <span v-if="label">
         {{ label }}
       </span>
-      <!--Label Prop-->
-      <span v-if="!label && $slots.label" class="inline-flex items-center">
+            <!--Label Prop-->
+            <span v-if="!label && $slots.label" class="inline-flex items-center">
         <!--Label Slot-->
         <slot name="label"/>
       </span>
-      <!--Sub Label-->
-      <span
-          v-if="subLabel"
-          :class="['text-xs italic',subLabelColors[subLabelColor]]"
-      >
+            <!--Sub Label-->
+            <span
+                v-if="subLabel"
+                :class="['text-xs italic',subLabelColors[subLabelColor]]"
+            >
         {{ subLabel }}
       </span>
-    </label>
-    <!--Input Content-->
-    <slot></slot>
-    <!--Error-->
-    <div v-show="errors">
-      <template v-for="error in errors" :key="error.$uid">
-        <p class="text-sm text-red-600 mt-2 whitespace-nowrap">
-          {{ error.$message }}
-        </p>
-      </template>
+        </label>
+        <!--Input Content-->
+        <slot></slot>
+        <!--Error-->
+        <div v-show="errors">
+            <template v-for="error in errors" :key="error.$uid">
+                <p class="text-sm text-red-600 mt-2 whitespace-nowrap">
+                    {{ error.$message }}
+                </p>
+            </template>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -40,12 +40,35 @@ import {defineComponent, provide, toRefs} from 'vue'
 import {inputGroupStyleMixin} from "@/Mixins/Styles/inputGroupStyleMixin";
 
 export default defineComponent({
-  props: ['label', 'subLabel', 'subLabelColor', 'labelFor', 'errors'],
-  mixins: [inputGroupStyleMixin],
-  setup(props){
-    /*Error Status Provide to Input Component*/
-    const {errors} = toRefs(props);
-    provide('errors', errors );
-  }
+    props: {
+        label: {
+            type: String,
+            default: null
+        },
+        subLabel: {
+            type: String,
+            default: null
+        },
+        subLabelColor: {
+            type: String,
+            default: 'yellow'
+        },
+        labelFor: {
+            type: String,
+            default: null
+        },
+        errors: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    },
+    mixins: [inputGroupStyleMixin],
+    setup(props) {
+        /*Error Status Provide to Input Component*/
+        const {errors} = toRefs(props);
+        provide('errors', errors);
+    }
 })
 </script>
