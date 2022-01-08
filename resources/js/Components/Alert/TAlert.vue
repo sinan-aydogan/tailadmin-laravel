52dc52290f71a077904760ff056f2c1d6a8f1121
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-08 03:56:47 +0300

    adds dark mode support

--- resources/js/Components/Alert/TAlert.vue
+++ resources/js/Components/Alert/TAlert.vue
@@ -1,164 +1,167 @@
 <template>
-  <transition name="alert">
-    <!--Alert Container-->
-    <div
-      v-if="showAlertBox"
-      :class="tStyle['container']"
-    >
-      <!--Inline Line-->
-      <div v-if="design.includes('line')"
-           id="line"
-           :class="tStyle['line']"
-      />
+    <transition name="alert">
+        <!--Alert Container-->
+        <div
+            v-if="showAlertBox"
+            class="alert"
+            :class="[
+                `alert-${design}`,
+                !type ? `alert-${design}-${color}` :
+                    [{ 'alert-green': type === 'success' },
+                    { 'alert-yellow': type === 'warning' },
+                    { 'alert-red': type === 'error' },
+                    { 'alert-blue': type === 'info' }],
+                `radius-${radius}`,
+            ]"
+        >
+            <!--Alert Line-->
+            <div v-if="design.includes('line')" class="alert-line" />
 
-      <!--Alert Icon-->
-      <div v-if="hasSlot('icon') && design !== 'elegant'"
-           class="alert-icon"
-      >
-        <slot name="icon"></slot>
-      </div>
+            <!--Alert Icon-->
+            <div v-if="hasSlot('icon')" class="alert-icon">
+                <slot name="icon"></slot>
+            </div>
 
+            <!--Alert Content-->
+            <div class="alert-content">
+                <!--Title-->
+                <span v-if="title" class="alert-title" v-text="title"></span>
+                <!--Content-->
+                <span>
+                    <slot></slot>
+                </span>
+            </div>
 
-      <!--Alert Content-->
-      <div class="alert-content-container">
-        <!--Elegant Title-->
-        <div v-if="title && design === 'elegant'" id="elegant-title">
-          {{ title }}
+            <!--Close Icon-->
+            <icon icon="times" v-if="closeable" @click="close" class="alert-close" />
+
+            <!--Countdown Line-->
+            <div v-if="timer" class="alert-countdown">
+                <div
+                    class="alert-countdown-line"
+                    :class="`radius-${radius}`"
+                    :style="{ width: countDownCounter + '%' }"
+                ></div>
+            </div>
         </div>
-        <div class="alert-content">
-          <!--General Title (except elegant style)-->
-          <span v-if="title && design !== 'elegant'" class="alert-title">
-            {{ title }}
-          </span>
-          <!--Content Slot-->
-          <div>
-            <slot></slot>
-          </div>
-        </div>
-      </div>
-
-      <!--Close Icon-->
-      <span v-if="closeable" class="alert-close" @click="close">
-        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </span>
-
-      <!--Countdown Line-->
-      <div v-if="timer" class="alert-countdown">
-        <div id="countdown" :style="{width: countDownCounter+'%'}"></div>
-      </div>
-    </div>
-  </transition>
+    </transition>
 </template>
 <script>
-import { defineComponent, ref, toRefs, computed, reactive } from "vue";
+/* Main Functions */
+import { defineComponent, ref, toRefs, watch } from "vue";
+
+/*Import FontAwesomeIcon*/
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+library.add(faTimes)
 
 export default defineComponent({
-  name: "TAlert",
-  props: {
-    id: {
-      type: String,
-      default: function() {
-        return "alert-" + Number(new Date());
-      }
-    },
-    closeable: {
-      type: Boolean,
-      default: false
-    },
-    timer: {
-      type: Number,
-      default: null,
-      required: false
-    },
-    title: {
-      type: String,
-      default: null
-    },
-    radius: {
-      type: Number,
-      default: 3
-    },
-    design: {
-      type: String,
-      default: "filled"
-    },
-    color: {
-      type: String,
-      default: "white"
-    }
-  },
-  emits: ["destroy"],
-
-  setup(props, { slots, emit }) {
-    /*Definitions*/
-    const { design, color, radius, timer, id } = toRefs(props);
-    const showAlertBox = ref(true);
-
-    /*Generating Style Classes*/
-    const tStyle = reactive({});
-    tStyle["container"] = computed(() => {
-      return "alert " +
-        "alert-" + design.value + "-" + color.value + " " +
-        "alert-" + design.value + "-base" + " " +
-        "radius-" + radius.value;
-    });
-    tStyle["line"] = computed(() => {
-      return (design.value === "inline" ? "alert-inline-line" : "alert-outline-line");
-    });
-
-    /*Close Function*/
-    const close = () => {
-      showAlertBox.value = false;
-      emit("destroy", id.value);
-    };
-
-    /*Timer*/
-    const timerCounter = ref(0);
-    const countDownCounter = ref(0);
-
-    if (timer.value) {
-      /*Timer Function*/
-      setTimeout(() => {
-        showAlertBox.value = false;
-        emit("destroy", id.value);
-      }, timer.value);
-
-      /*Count Down Function*/
-      let countDownFn = setInterval(() => {
-        if (timer.value >= timerCounter.value) {
-          countDownCounter.value = 100 - (timerCounter.value / timer.value) * 100;
-          timerCounter.value += 4;
-        } else {
-          clearInterval(countDownFn);
+    name: "TAlert",
+    props: {
+        id: {
+            type: String,
+            default: function () {
+                return "alert-" + Number(new Date());
+            }
+        },
+        closeable: {
+            type: Boolean,
+            default: false
+        },
+        timer: {
+            type: Number,
+            default: null,
+            required: false
+        },
+        title: {
+            type: String,
+            default: null
+        },
+        type: {
+            type: String,
+            default: null
+        },
+        radius: {
+            type: Number,
+            default: 3
+        },
+        design: {
+            type: String,
+            default: "filled"
+        },
+        color: {
+            type: String,
+            default: "white"
         }
-      }, 1);
+    },
+    emits: ["destroy"],
+
+    setup(props, { slots, emit }) {
+        /*Definitions*/
+        const { timer, id } = toRefs(props);
+        const showAlertBox = ref(true);
+
+        /*Close Function*/
+        const close = () => {
+            showAlertBox.value = false;
+            emit("destroy", id.value);
+        };
+
+        /*Timer*/
+        const timerCounter = ref(0);
+        const countDownCounter = ref(0);
+
+        watch(timer, (newValue, oldValue) => {
+            if (!oldValue && newValue > 0) {
+                /*Timer Function*/
+                setTimeout(() => {
+                    showAlertBox.value = false;
+                    emit("destroy", id.value);
+                }, timer.value);
+
+                /*Count Down Function*/
+                let countDownFn = setInterval(() => {
+                    if (timer.value >= timerCounter.value) {
+                        countDownCounter.value = 100 - (timerCounter.value / timer.value) * 100;
+                        timerCounter.value += 4;
+                    } else {
+                        clearInterval(countDownFn);
+                    }
+                }, 1);
+            }
+        })
+
+        /*Slot Check*/
+        const hasSlot = name => !!slots[name];
+
+        return {
+            showAlertBox,
+            countDownCounter,
+            timerCounter,
+            hasSlot,
+            close
+        };
     }
-
-    /*Slot Check*/
-    const hasSlot = name => !!slots[name];
-
-    return { showAlertBox, tStyle, countDownCounter, timerCounter, hasSlot, close };
-  }
 });
 </script>
 
 <style scoped>
 /* eslint-disable no-alert */
-.alert-enter-active, .alert-leave-active {
-  transition: opacity ease-out .75s;
+.alert-enter-active,
+.alert-leave-active {
+    transition: opacity ease-out 0.75s;
 }
 
-.alert-enter, .alert-leave-to {
-  opacity: 0;
-  height: revert;
+.alert-enter,
+.alert-leave-to {
+    opacity: 0;
+    height: revert;
 }
 
-.alert-enter-to, .alert-leave {
-  opacity: 1;
-  height: revert;
+.alert-enter-to,
+.alert-leave {
+    opacity: 1;
+    height: revert;
 }
 
 /* eslint-disable no-alert */
--- resources/js/Pages/Samples/Components/Alert.vue
+++ resources/js/Pages/Samples/Components/Alert.vue
@@ -1,71 +1,48 @@
 <template>
-  <Head title="Alerts" />
-  <app-layout>
-    <template #header>Alerts</template>
-    <template #subHeader>Closeable and timeable elite alert boxes</template>
-    <template #default>
-      <grid-section :col="1" :gap="4">
-        <t-alert
-          v-for="(content,key) in demoContents"
-          :design="content.design"
-          :color="content.color"
-          :radius="content.radius"
-          :title="content.title"
-          :closeable="content.closeable"
-          :key="key"
-        >
-          <template #icon v-if="content.icon || content.media">
-              <icon v-if="content.icon" :icon="content.icon" size="lg"/>
-            <t-avatar
-              v-if="content.media"
-              :radius="8"
-              :size="3"
-              src="https://i.pravatar.cc/300"
-            />
-          </template>
-          <span v-html="content.content"></span>
-        </t-alert>
-      </grid-section>
-
-      <grid-section :col="1">
-        <t-alert id="test"
-                 :timer="4000"
-                 design="inline"
-                 title="timed alert"
-                 color="pink"
-        >
-          <template #icon>
-            <icon icon="clock" size="lg"/>
-          </template>
-          Closer timer activated, alert box. (<b>4 seconds</b>)
-        </t-alert>
-        <component
-          :is="'t-alert'"
-          v-if="newAlertID>0"
-          id="alertTimer"
-          :key="newAlertID"
-          :timer="4000"
-          design="inline"
-          title="timed alert"
-          color="pink"
-          @destroy="$event === 'alertTimer' ? active = false : active = true"
-        >
-          <template #icon>
-            <icon icon="clock" size="lg"/>
-          </template>
-          Closer timer activated, alert box. (<b>4 seconds</b>)
-        </component>
-        <t-button color="pink" size="full" @click="addAlertBox">
-          Recall Closed Alert Box
-        </t-button>
-      </grid-section>
-    </template>
-  </app-layout>
+    <Head title="Alerts" />
+    <app-layout>
+        <template #header>{{ tm('pageTitle') }}</template>
+        <template #subHeader>{{ tm('pageSubTitle') }}</template>
+        <template #action-buttons>
+            <t-button @click="addAlertBox">
+                <icon icon="play" />
+                <span v-text="tm('activateTimer')" />
+            </t-button>
+        </template>
+        <template #default>
+            <grid-section :col="1" :gap="4">
+                <template v-for="(content,key) in demoContents" :key="key+newAlertID">
+                    <t-alert
+                        :id="key + newAlertID"
+                        :design="content.design"
+                        :color="content.color"
+                        :radius="content.radius"
+                        :title="content.title ? tm(content.title) : null"
+                        :closeable="content.closeable"
+                        :timer="content.timer"
+                        @destroy="$event === key + newAlertID ? active = false : active = true"
+                    >
+                        <template #icon v-if="content.icon || content.media">
+                            <icon v-if="content.icon" :icon="content.icon" size="lg" />
+                            <t-avatar
+                                v-if="content.media"
+                                :radius="8"
+                                :size="3"
+                                src="https://i.pravatar.cc/300"
+                                class="my-2"
+                            />
+                        </template>
+                        <span v-text="tm(content.content)"></span>
+                    </t-alert>
+                </template>
+            </grid-section>
+        </template>
+    </app-layout>
 </template>
 
 <script>
 /*Main Functions*/
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import { Head } from "@inertiajs/inertia-vue3";
 
 /*Component*/
@@ -77,106 +54,157 @@ import TAvatar from "@/Components/Avatar/TAvatar";
 
 /*Import FontAwesomeIcon*/
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {faCheckCircle, faTrash, faChevronLeft, faRetweet, faBan, faClock, faInfo} from "@fortawesome/free-solid-svg-icons";
-library.add(faCheckCircle, faTrash, faChevronLeft, faRetweet, faBan, faClock, faInfo)
+import { faCheckCircle, faTrash, faChevronLeft, faRetweet, faBan, faClock, faInfo, faHistory, faPlay } from "@fortawesome/free-solid-svg-icons";
+library.add(faCheckCircle, faTrash, faChevronLeft, faRetweet, faBan, faClock, faInfo, faHistory, faPlay)
 
+/* Multi-language */
+import { useI18n } from "vue-i18n";
 
 export default defineComponent({
-  name: "Alert",
-  components: {
-    Head,
-    TAvatar,
-    AppLayout,
-    GridSection,
-    TAlert,
-    TButton,
-  },
-  setup() {
-    const demoContents = {
-      "success": {
-        design: "filled",
-        content: "The post added successfully <b>(filled)</b>",
-        color: "green",
-        radius: 3,
-        icon: "check-circle",
-        title: "Success",
-        timer: 10000
-      },
-      "danger": {
-        design: "light",
-        content: "The user deleted successfully <b>(light)</b>",
-        color: "red", radius: 3,
-        icon: "trash",
-        title: "Danger",
-        closeable: true,
-        timer: 9000
-      },
-      "warning": {
-        design: "inline",
-        content: "Your account was blocked, please connect with IT <b>(inline)</b>",
-        color: "yellow",
-        radius: 3,
-        icon: "ban",
-        timer: 8000
-      },
-      "info": {
-        design: "outline",
-        content: "You entered a new configuration, the settings will refresh 24hr after <b>(outline)</b>",
-        color: "indigo",
-        radius: 2,
-        icon: "retweet",
-        timer: 7000
-      },
-      "gradient": {
-        design: "gradient",
-        content: "The new user is successfully added.<br>\n<b>User:</b> Hamdi KAYA <b>(gradient)</b>",
-        color: "dream",
-        radius: 3,
-        media: true,
-        closeable: true,
-        timer: 6000
-      },
-      "elegant": {
-        design: "elegant",
-        content: "The all of changes overwrite to themself <b>(elegant)</b>",
-        color: "purple",
-        radius: 3,
-        title: "Attention",
-        closeable: true,
-        timer: 5000
-      },
-      "block": {
-        design: "block",
-        content: "Please, check your task list <b>(block)</b>",
-        color: "blue",
-        radius: 1,
-        icon: "info",
-        closeable: true,
-        timer: 4000
-      }
-    };
-    const demoDesigns = ["filled", "light", "gradient", "inline", "outline", "elegant", "block"];
-    const active = ref(false)
-    const newAlertID = ref(0)
-    const selectedColor = ref('red')
-    const colors = ["red", "blue", "green", "yellow", "indigo", "pink", "purple", "gray", "black", "white"]
+    name: "Alert",
+    components: {
+        Head,
+        TAvatar,
+        AppLayout,
+        GridSection,
+        TAlert,
+        TButton,
+    },
+    setup() {
+        /* Multi-language */
+        const { t, tm } = useI18n({
+            inheritLocale: true,
+            messages: {
+                en: {
+                    pageTitle: 'Alerts',
+                    pageSubTitle: 'Closeable and timeable elite alert boxes',
+                    filledTitle: 'Success',
+                    filledContent: 'The post added successfully (filled)',
+                    lightTitle: 'Danger',
+                    lightContent: 'The user deleted successfully (light)',
+                    inlineContent: 'Your account was blocked, please connect with IT (inline)',
+                    outlineContent: 'You entered a new configuration, the settings will refresh 24hr after (outline)',
+                    gradientContent: 'The new user is successfully added. \n User: Hamdi KAYA (gradient)',
+                    elegantTitle: 'Attention',
+                    elegantContent: 'The all of changes overwrite to themself (elegant)',
+                    blockContent: 'Please, check your task list (block)',
+                    activateTimer: 'Activate Timer to Close'
+                },
+                tr: {
+                    pageTitle: 'Uyarı Kutusu',
+                    pageSubTitle: 'Gizlenebilir ve zamanlanabilir şık uyarı kutuları',
+                    filledTitle: 'İşlem Başarılı',
+                    filledContent: 'Gönderiniz başarıyla yayınlandı (filled)',
+                    lightTitle: 'Dikkat',
+                    lightContent: 'Kullanıcı silindi (light)',
+                    inlineContent: 'Hesabınız bloke oldu, lütfen IT ile iletişime geçiniz (inline)',
+                    outlineContent: 'Yeni girdiğiniz ayarlar 24 saat içinde aktif olacaktır (outline)',
+                    gradientContent: 'Yeni kullanıcı eklendi. \n Kullanıcı: Hamdi KAYA (gradient)',
+                    elegantTitle: 'Uyarı',
+                    elegantContent: 'Yapıtığınız değişiklikler, var olan kayıtların üzerine yazılacak (elegant)',
+                    blockContent: 'Lütfen görev listenizi kontrol ediniz (block)',
+                    activateTimer: 'Kapatma Zamanlayıcısını Çalıştır'
+                },
+            },
+        });
 
-    const addAlertBox = () => {
-      if (active.value === false) {
-        newAlertID.value++;
-        active.value = true;
-      }
-    }
+        const dynamicTimer = computed(() => {
+            return active.value ? 5000 : null
+        })
 
-    return {
-        active,
-        newAlertID,
-        selectedColor,
-        colors,
-        demoContents,
-        demoDesigns,
-        addAlertBox
+        /* Demo Contents */
+        const demoContents = ref({
+            "success": {
+                design: "filled",
+                content: "filledContent",
+                color: "success",
+                closeable: true,
+                radius: 3,
+                icon: "check-circle",
+                title: "filledTitle",
+                timer: dynamicTimer
+            },
+            "danger": {
+                design: "light",
+                content: "lightContent",
+                color: "dark",
+                radius: 3,
+                icon: "trash",
+                title: "lightTitle",
+                closeable: true,
+                timer: dynamicTimer
+            },
+            "warning": {
+                design: "inline",
+                content: "inlineContent",
+                color: "warning",
+                radius: 3,
+                icon: "ban",
+                timer: dynamicTimer
+            },
+            "info": {
+                design: "outline",
+                content: "outlineContent",
+                color: "info",
+                radius: 2,
+                icon: "retweet",
+                timer: dynamicTimer
+            },
+            "gradient": {
+                design: "gradient",
+                content: "gradientContent",
+                color: "dream",
+                radius: 3,
+                media: true,
+                closeable: true,
+                timer: dynamicTimer
+            },
+            "elegant": {
+                design: "elegant",
+                content: "elegantContent",
+                color: "violet",
+                radius: 3,
+                title: "elegantTitle",
+                closeable: true,
+                timer: dynamicTimer
+            },
+            "block": {
+                design: "block",
+                content: "blockContent",
+                color: "danger",
+                radius: 1,
+                icon: "info",
+                closeable: true,
+                timer: dynamicTimer
+            }
+        });
+        const demoDesigns = ["filled", "light", "gradient", "inline", "outline", "elegant", "block"];
+        const active = ref(false)
+        const newAlertID = ref(1)
+        const selectedColor = ref('red')
+        const colors = ["red", "blue", "green", "yellow", "indigo", "pink", "purple", "gray", "black", "white"]
+
+
+
+        const addAlertBox = () => {
+            if (active.value === false) {
+                active.value = true;
+                setTimeout(() => {
+                    newAlertID.value++
+                }, 6000)
+            }
+        }
+
+        return {
+            active,
+            newAlertID,
+            selectedColor,
+            colors,
+            demoContents,
+            demoDesigns,
+            addAlertBox,
+            t, tm,
         };
-  }
+    }
 });
 </script>
