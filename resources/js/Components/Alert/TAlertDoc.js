31df7e364bacc0fafef4548f9327e63849caa566
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-11 15:20:32 +0300

    add new js and template samples

--- resources/js/Components/Alert/TAlertDoc.js
+++ resources/js/Components/Alert/TAlertDoc.js
@@ -18,7 +18,8 @@ export default {
     design="filled"
     color="success"
     title="Success"
-    closeable="true"
+    :radius=1
+    :closeable="true"
     >
     <template #icon>
         <icon icon="check-circle" size="lg" />
@@ -45,7 +46,7 @@ export default {
     design="light"
     color="danger"
     title="Danger"
-    closeable="true"
+    :closeable="true"
     >
     <template #icon>
         <icon icon="trash" size="lg" />
@@ -71,14 +72,116 @@ export default {
     id="blocked"
     design="inline"
     color="warning"
-    closeable="false"
+    :closeable="false"
     >
     <template #icon>
         <icon icon="ban" size="lg" />
     </template>
     The user deleted successfully (light)
 </t-alert>`,
-    }
-}
+    },
+    outline: {
+        js: `/* FontAwesomeIcon Import */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faRetweet } from "@fortawesome/free-solid-svg-icons";
+library.add( faRetweet )
 
-export { samples }
+/* Alert Component Import */
+import TAlert from '@/Components/Alert/TAlert.vue'
+
+export default {
+    components: { TAlert },
+    setup() {
+    },
+}`,
+        template: `<t-alert
+    id="updateSettings"
+    design="outline"
+    color="info"
+    :closeable="false"
+    >
+    <template #icon>
+        <icon icon="retweet" size="lg" />
+    </template>
+    You entered a new configuration, the settings will refresh 24hr after (outline)
+</t-alert>`,
+    },
+    gradient: {
+        js: `/* Alert Component Import */
+import TAlert from '@/Components/Alert/TAlert.vue'
+
+/* Avatar Component Import */
+import TAvatar from "@/Components/Avatar/TAvatar";
+
+export default {
+    components: {
+        TAlert,
+        TAvatar
+    },
+    setup() {
+    },
+}`,
+        template: `<t-alert
+    id="createSuccess"
+    design="gradient"
+    color="dream"
+    :closeable="true"
+    >
+    <t-avatar
+        :radius="8"
+        :size="3"
+        src="https://i.pravatar.cc/300"
+        />
+    The new user is successfully added.<br>
+    User: Hamdi KAYA (gradient)
+</t-alert>`,
+    },
+    elegant: {
+        js: `/* Alert Component Import */
+import TAlert from '@/Components/Alert/TAlert.vue'
+
+export default {
+    components: { TAlert },
+    setup() {
+    },
+}`,
+        template: `<t-alert
+    id="overwrite"
+    design="elegant"
+    color="violet"
+    title="Attention"
+    :closeable="true"
+    :radius=8
+    >
+    The all of changes overwrite to themself (elegant)
+</t-alert>`,
+    },
+    block: {
+        js: `/* FontAwesomeIcon Import */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
+library.add( faInfo )
+
+/* Alert Component Import */
+import TAlert from '@/Components/Alert/TAlert.vue'
+
+export default {
+    components: { TAlert },
+    setup() {
+    },
+}`,
+        template: `<t-alert
+    id="blocked"
+    design="block"
+    color="info"
+    :closeable="true"
+    >
+    <template #icon>
+        <icon icon="info" size="lg" />
+    </template>
+    Please, check your task list (block)
+</t-alert>`,
+    }
+};
+
+export { samples };
