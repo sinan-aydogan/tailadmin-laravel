b01d7176733eb5ba98e57655e95951559f3ce502
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-12 01:32:11 +0300

    add language selector

--- resources/js/Layouts/TopMenu/TopMenu.vue
+++ resources/js/Layouts/TopMenu/TopMenu.vue
@@ -1,126 +1,129 @@
 <template>
-  <div
-    class="top-menu"
-    :class="'radius-' + conf.app.topMenu.radius"
-  >
-    <!--Left Menu Trigger-->
     <div
-      @click="$emit('foldLeftMenu', $event)"
-      class="trigger"
-      :class="'radius-' + conf.app.topMenu.radius"
+        class="top-menu"
+        :class="'radius-' + conf.app.topMenu.radius"
     >
-      <!--Fold & Close Icon-->
-      <svg
-        class="trigger-icon"
-        :class="[
+        <!--Left Menu Trigger-->
+        <div
+            @click="$emit('foldLeftMenu', $event)"
+            class="trigger"
+            :class="'radius-' + conf.app.topMenu.radius"
+        >
+            <!--Fold & Close Icon-->
+            <svg
+                class="trigger-icon"
+                :class="[
                 (showLeftMenu && !foldLeftMenu) || ((deviceType === 'phone' || deviceType==='tablet')&&showLeftMenu) ?
                 'trigger-icon-show' : 'trigger-icon-hide'
                 ]"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-      </svg>
-      <!--Open Icon-->
-      <svg
-        class="trigger-icon"
-        :class="[
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+            <!--Open Icon-->
+            <svg
+                class="trigger-icon"
+                :class="[
                 (showLeftMenu && foldLeftMenu) || !showLeftMenu ?
                 'trigger-icon-show' : 'trigger-icon-hide'
                 ]"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-      </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+            </svg>
+        </div>
+        <!--Search Box-->
+        <div class="search-box">
+            <div
+                @click="searchBar=true"
+                class="search-box-wrapper"
+                :class="'radius-' + conf.app.topMenu.radius"
+            >
+                <svg
+                    class="search-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                </svg>
+            </div>
+        </div>
+        <!--Active Team-->
+        <div class="active-team">
+            <div
+                v-if="$page.props.jetstream.hasTeamFeatures"
+                :title="$page.props.user.current_team.name"
+                class="active-team-wrapper">
+                <svg
+                    class="left-menu-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
+                </svg>
+                <span
+                    class="active-team-label"
+                    v-text="$page.props.user.current_team.name"
+                />
+            </div>
+        </div>
+        <!--Notifications-->
+        <top-menu-notification/>
+        <!--Theme Selector-->
+        <top-menu-theme-selector/>
+        <!--Language Selector-->
+        <top-menu-language-selector/>
+        <!--User Menu-->
+        <top-menu-user-menu/>
     </div>
-    <!--Search Box-->
-    <div class="search-box">
-      <div
-        @click="searchBar=true"
-        class="search-box-wrapper"
-        :class="'radius-' + conf.app.topMenu.radius"
-      >
-        <svg
-          class="search-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
-      </div>
-    </div>
-    <!--Active Team-->
-    <div class="active-team">
-      <div
-        v-if="$page.props.jetstream.hasTeamFeatures"
-        :title="$page.props.user.current_team.name"
-        class="active-team-wrapper">
-        <svg
-          class="left-menu-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-        </svg>
-        <span
-          class="active-team-label"
-          v-text="$page.props.user.current_team.name"
-        />
-      </div>
-    </div>
-    <!--Notifications-->
-    <top-menu-notification />
-      <!--Theme Selector-->
-      <top-menu-theme-selector/>
-    <!--User Menu-->
-    <top-menu-user-menu />
-  </div>
-  <teleport to="body">
-    <!--Search Bar-->
-    <t-loading v-model="searchBar" color="gray" title="Search" closeable>
-      <div
-        class="search-modal"
-        :class="'radius-' + conf.app.topMenu.radius"
-      >
-        <input type="text" id="search" :placeholder="conf.app.topMenu.searchPlaceHolderText" />
-      </div>
-    </t-loading>
-  </teleport>
+    <teleport to="body">
+        <!--Search Bar-->
+        <t-loading v-model="searchBar" color="gray" title="Search" closeable>
+            <div
+                class="search-modal"
+                :class="'radius-' + conf.app.topMenu.radius"
+            >
+                <input type="text" id="search" :placeholder="conf.app.topMenu.searchPlaceHolderText"/>
+            </div>
+        </t-loading>
+    </teleport>
 </template>
 
 <script>
 import TopMenuNotification from "@/Layouts/TopMenu/TopMenuNotification";
 import TopMenuUserMenu from "@/Layouts/TopMenu/TopMenuUserMenu";
-import { defineComponent, inject, ref } from "vue";
+import {defineComponent, inject, ref} from "vue";
 import TLoading from "@/Components/Loading/TLoading";
 import TopMenuThemeSelector from "@/Layouts/TopMenu/TopMenuThemeSelector";
+import TopMenuLanguageSelector from "@/Layouts/TopMenu/TopMenuLanguageSelector";
 
 export default defineComponent({
-  name: "TopMenu",
-  components: {TopMenuThemeSelector, TLoading, TopMenuUserMenu, TopMenuNotification },
-  emits: ["foldLeftMenu"],
-  setup() {
-    /*Definitions*/
-    const searchBar = ref(false);
-    /*Injections*/
-    const conf = inject("conf");
-    const deviceType = inject("deviceType");
-    const foldLeftMenu = inject("foldLeftMenu");
-    const showLeftMenu = inject("showLeftMenu");
+    name: "TopMenu",
+    components: {TopMenuLanguageSelector, TopMenuThemeSelector, TLoading, TopMenuUserMenu, TopMenuNotification},
+    emits: ["foldLeftMenu"],
+    setup() {
+        /*Definitions*/
+        const searchBar = ref(false);
+        /*Injections*/
+        const conf = inject("conf");
+        const deviceType = inject("deviceType");
+        const foldLeftMenu = inject("foldLeftMenu");
+        const showLeftMenu = inject("showLeftMenu");
 
-    return { conf, deviceType, foldLeftMenu, showLeftMenu, searchBar };
-  }
+        return {conf, deviceType, foldLeftMenu, showLeftMenu, searchBar};
+    }
 });
 </script>
 
---
+++ resources/js/Layouts/TopMenu/TopMenuLanguageSelector.vue
@@ -0,0 +1,66 @@
+<template>
+    <t-dropdown align="right" trigger-type="rich">
+        <template #trigger>
+            <!--Selected Language-->
+            <div
+                class="top-menu-dropdown-trigger-transparent"
+                :class="'radius-' + conf.app.topMenu.radius"
+            >
+                <component
+                    :is="languages.find(i=>i.id === locale).flag"
+                    class="w-6 aspect-auto drop-shadow transform hover:scale-110 active:scale-90 transition-all duration-300"
+                    :alt="languages.find(i=>i.id === locale).name"
+                />
+            </div>
+        </template>
+        <template #default>
+            <!--Language Lists-->
+            <div class="top-menu-dropdown-content-wrapper-transparent">
+                <template v-for="lang in languages" :key="lang.id">
+                    <div @click="changeLang(lang.id)" class="top-menu-dropdown-item-transparent">
+                        <component
+                            :is="lang.flag"
+                            class="w-6 aspect-auto drop-shadow"
+                        />
+                        <span v-text="lang.name"></span>
+                    </div>
+                </template>
+            </div>
+        </template>
+    </t-dropdown>
+</template>
+
+<script>
+/*Main functions*/
+import {defineComponent, inject} from "vue";
+import langChooserFn from "@/Functions/langChooser"
+
+/*Components*/
+import TDropdown from "@/Components/Dropdown/TDropdown";
+
+/*Sources*/
+import {languages, flags} from "@/Lang/languages";
+
+export default defineComponent({
+    name: "TopMenuLanguageSelector",
+    components: {
+        TDropdown,
+        ...flags
+    },
+    setup() {
+        const conf = inject("conf");
+        const {locale, changeLang} = langChooserFn()
+
+        return {
+            conf,
+            languages,
+            locale,
+            changeLang
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
