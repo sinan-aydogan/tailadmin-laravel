cd9b2345fb7ba92ae808ab552e29039bd1ec4775
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-11 21:49:30 +0300

    adds table feature

--- resources/js/Components/Code/TCodeShowcase.vue
+++ resources/js/Components/Code/TCodeShowcase.vue
@@ -41,6 +41,53 @@
                     <pre v-if="activeTab === tab.id && tab.type === 'code'" v-highlightjs>
                         <code class="rounded-b overflow-scroll" :class="tab.codeLang"><slot :name="tab.id"></slot></code>
                     </pre>
+
+                    <!-- Table -->
+                    <div
+                        v-if="activeTab === tab.id && tab.type === 'table'"
+                        class="p-2 space-y-2 bg-slate-200 dark:bg-slate-800 rounded-b"
+                    >
+                        <!-- Content -->
+                        <div
+                            v-for="row in tab.table.content"
+                            :key="row"
+                            class="bg-slate-300 border border-slate-400 dark:border-transparent dark:bg-slate-500 rounded p-2"
+                        >
+                            <!-- Row -->
+                            <template v-for="header in tab.table.headers" :key="header">
+                                <div
+                                    v-if="row[header.id]"
+                                    class="flex p-1 group border-b last:border-0 border-slate-600 hover:bg-slate-500 hover:-translate-y-[.2rem] dark:hover:bg-slate-600 hover:text-slate-100 hover:rounded hover:border-transparent"
+                                    :class="header.id === 'description' ? 'flex-col' : 'justify-between'"
+                                >
+                                    <!-- Header -->
+                                    <span
+                                        class="float-left after:content-[':'] font-semibold"
+                                    >{{ header.label }}</span>
+
+                                    <!-- Regular Field -->
+                                    <span
+                                        v-if="(typeof row[header.id] !== 'object')"
+                                        :class="{
+                                            'italic bg-slate-400/50 rounded p-2': header.id === 'description'
+                                        }"
+                                    >{{ row[header.id] }}</span>
+
+                                    <!-- Object and Array Field -->
+                                    <span v-else class="pl-10 text-right">
+                                        <span
+                                            v-for="item in row[header.id]"
+                                            :key="item"
+                                            class="mr-1 last:mr-0 select-all"
+                                            :class="{
+                                                'font-semibold bg-slate-200/50 border border-sky-800/20 group-hover:bg-slate-300 group-hover:text-slate-700 dark:bg-blue-100/50 px-1 rounded': header.colorfulBg
+                                            }"
+                                        >{{ item }}</span>
+                                    </span>
+                                </div>
+                            </template>
+                        </div>
+                    </div>
                 </template>
             </div>
         </transition>
