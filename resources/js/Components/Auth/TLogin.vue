d205aff082fcfb62a867e920eee808b3ab06e16f
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-14 01:23:08 +0300

    adds change button for bg image and dark mode

--- resources/js/Components/Auth/TLogin.vue
+++ resources/js/Components/Auth/TLogin.vue
@@ -1,232 +1,309 @@
 <template>
-  <full-screen-layout :bg-image="bgImage" :color="bgColor" :gradient-direction="bgGradientDirection">
-    <div
-      :class="[
+    <full-screen-layout :bg-image="bgImage" :color="bgColor" :gradient-direction="bgGradientDirection">
+        <div
+            :class="[
                 'relative',
                 deviceType === 'phone' && 'w-full'
                 ]"
-    >
-      <!--Container-->
-      <div :class="[
+        >
+            <!--Container-->
+            <div :class="[
             'auth-container',
             deviceType !== 'phone' && radiusStyle
             ]">
-        <!--Header-->
-        <div :class="[
+                <!--Header-->
+                <div class="bg-opacity-50 backdrop-filter backdrop-blur" :class="[
                 'auth-header',
                 calculatedLoginStyle
                 ]">
-          <!--Logo-->
-          <div class="auth-logo">
-            <slot name="logo" />
-          </div>
-          <!--Greeting-->
-          <div class="auth-greeting">
-            <div class="text-sm">
-              <!--Status-->
-              <div v-if="status" class="auth-status">
-                {{ status }}
-              </div>
-              <slot v-else name="greeting" />
-            </div>
-          </div>
-        </div>
+                    <!--Logo-->
+                    <div class="auth-logo">
+                        <slot name="logo"/>
+                    </div>
+                    <!--Greeting-->
+                    <div class="auth-greeting">
+                        <div class="text-sm">
+                            <!--Status-->
+                            <div v-if="status" class="auth-status">
+                                {{ status }}
+                            </div>
+                            <slot v-else name="greeting"/>
+                        </div>
+                    </div>
+                </div>
 
-        <!--Form-->
-        <div class="auth-form">
-          <form @submit.prevent="submit">
-            <!--Email-->
-            <div>
-              <t-input-group label="Email" label-for="email">
-                <t-input-text
-                  id="email"
-                  v-model="form.email"
-                  :radius="3"
-                  autofocus
-                  autocomplete="username"
-                  required
-                  type="email"
-                />
-              </t-input-group>
-            </div>
-            <!--Password-->
-            <div class="mt-4">
-              <t-input-group label="Password" label-for="password">
-                <t-input-text
-                  id="password"
-                  v-model="form.password"
-                  :radius="3"
-                  autocomplete="current-password"
-                  required
-                  type="password"
-                />
-              </t-input-group>
-            </div>
-            <div class="auth-remember">
-              <!--Remember Me-->
-              <label class="flex items-center">
-                <input
-                  v-model="form.remember"
-                  name="remember"
-                  type="checkbox"
-                />
-                <span class="auth-remember-text">Remember me</span>
-              </label>
-              <!--Forgot Password-->
-              <Link
-                v-if="canResetPassword"
-                :href="route('password.request')"
-                class="auth-forgot-password"
-              >
-                Forgot your password?
-              </Link>
-            </div>
-            <!--Submit Area-->
-            <div class="auth-submit-area">
-              <!--Register Button-->
-              <t-button
-                v-if="registerButton"
-                :class="{ 'opacity-25': form.processing }"
-                :design="registerButtonDesign"
-                :color="registerButtonColor"
-                :link="route('register')"
-                :radius="3"
-                type="link"
-              >
-                Register
-              </t-button>
+                <!--Form-->
+                <div class="auth-form">
+                    <form @submit.prevent="submit">
+                        <!--Email-->
+                        <div>
+                            <t-input-group label="Email" label-for="email">
+                                <t-input-text
+                                    id="email"
+                                    v-model="form.email"
+                                    :radius="3"
+                                    autofocus
+                                    autocomplete="username"
+                                    required
+                                    type="email"
+                                />
+                            </t-input-group>
+                        </div>
+                        <!--Password-->
+                        <div class="mt-4">
+                            <t-input-group label="Password" label-for="password">
+                                <t-input-text
+                                    id="password"
+                                    v-model="form.password"
+                                    :radius="3"
+                                    autocomplete="current-password"
+                                    required
+                                    type="password"
+                                />
+                            </t-input-group>
+                        </div>
+                        <div class="auth-remember">
+                            <!--Remember Me-->
+                            <label class="flex items-center">
+                                <input
+                                    v-model="form.remember"
+                                    name="remember"
+                                    type="checkbox"
+                                />
+                                <span class="auth-remember-text">Remember me</span>
+                            </label>
+                            <!--Forgot Password-->
+                            <Link
+                                v-if="canResetPassword"
+                                :href="route('password.request')"
+                                class="auth-forgot-password"
+                            >
+                                Forgot your password?
+                            </Link>
+                        </div>
+                        <!--Submit Area-->
+                        <div class="auth-submit-area">
+                            <!--Register Button-->
+                            <t-button
+                                v-if="registerButton"
+                                :class="{ 'opacity-25': form.processing }"
+                                :design="registerButtonDesign"
+                                :color="registerButtonColor"
+                                :link="route('register')"
+                                :radius="3"
+                                type="link"
+                            >
+                                Register
+                            </t-button>
 
-              <!--Submit Button-->
-              <t-button
-                :class="{ 'opacity-25': form.processing }"
-                :color="loginButtonColor"
-                :design="loginButtonDesign"
-                :disabled="form.processing"
-                :radius="3" class="ml-4"
-              >
-                Login
-              </t-button>
+                            <!--Submit Button-->
+                            <t-button
+                                :class="{ 'opacity-25': form.processing }"
+                                :color="loginButtonColor"
+                                :design="loginButtonDesign"
+                                :disabled="form.processing"
+                                :radius="3" class="ml-4"
+                            >
+                                Login
+                            </t-button>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </form>
+            <div class="auth-error">
+                <!--Errors-->
+                <transition @before-enter="beforeStyle" @after-enter="enterStyle">
+                    <t-alert v-if="hasErrors" :radius="deviceType !== 'phone' && 5" color="red">
+                        <template #icon>
+                            <t-bell-icon class="w-8 h-8"/>
+                        </template>
+                        <ul class="list-inside text-sm">
+                            <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+                        </ul>
+                    </t-alert>
+                </transition>
+            </div>
         </div>
-      </div>
-      <div class="auth-error">
-        <!--Errors-->
-        <transition @before-enter="beforeStyle" @after-enter="enterStyle">
-          <t-alert v-if="hasErrors" :radius="deviceType !== 'phone' && 5" color="red">
-            <template #icon>
-              <t-bell-icon class="w-8 h-8" />
-            </template>
-            <ul class="list-inside text-sm">
-              <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
-            </ul>
-          </t-alert>
-        </transition>
-      </div>
-    </div>
-  </full-screen-layout>
+    </full-screen-layout>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { loginStyleMixin } from "@/Mixins/Styles/loginStyleMixin";
+/*Main functions*/
+import {defineComponent,computed, ref} from "vue";
+import {loginStyleMixin} from "@/Mixins/Styles/loginStyleMixin";
+import {Link, useForm} from "@inertiajs/inertia-vue3";
+import windowSizeCalculator from "@/Functions/windowSizeCalculator";
+import darkModeFn from "@/Functions/darkMode";
+import useVuelidate from "@vuelidate/core";
+import {email, required} from "@vuelidate/validators";
+
+
+/*Components*/
 import TAlert from "@/Components/Alert/TAlert";
 import TBellIcon from "@/Components/Icon/TBellIcon";
 import TButton from "@/Components/Button/TButton";
 import FullScreenLayout from "@/Layouts/FullScreenLayout";
 import TInputGroup from "@/Components/Form/TInputGroup";
 import TInputText from "@/Components/Form/Inputs/TInputText";
-import { Link } from "@inertiajs/inertia-vue3";
-import windowSizeCalculator from "@/Functions/windowSizeCalculator";
+
+/*Sources*/
+import {appConf, authScreenConf} from "@/config";
+import {authBgImages} from "@/Sources/bgImages";
+
+/* Multi language */
+import {useI18n} from "vue-i18n";
+import langChooserFn from "@/Functions/langChooser"
+import {languages, flags, authTranslates} from "@/Lang/languages";
+
+/*Fontawesome icons*/
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faSun, faMoon, faPalette, faRedo} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSun, faMoon, faPalette, faRedo)
 
 export default defineComponent({
-  name: "TLogin",
-  components: {
-    TAlert,
-    TBellIcon,
-    TButton,
-    FullScreenLayout,
-    TInputGroup,
-    TInputText,
-    Link
-  },
-  mixins: [loginStyleMixin],
-  props: {
-    canResetPassword: Boolean,
-    status: String,
-    registerButtonDesign: {
-      type: String,
-      default: "filled"
+    name: "TLogin",
+    components: {
+        TAlert,
+        TBellIcon,
+        TButton,
+        FullScreenLayout,
+        TInputGroup,
+        TInputText,
+        Link,
+        ...flags,
     },
-    registerButtonColor: {
-      type: String,
-      default: "white"
+    mixins: [loginStyleMixin],
+    props: {
+        canResetPassword: Boolean,
+        status: String,
+        registerButtonDesign: {
+            type: String,
+            default: "filled"
+        },
+        design: {
+            type: Object,
+            default: null,
+        },
+        registerButtonColor: {
+            type: String,
+            default: "white"
+        },
+        loginButtonDesign: {
+            type: String,
+            default: "filled"
+        },
+        loginButtonColor: {
+            type: String,
+            default: "green"
+        },
+        bgColor: {
+            type: String,
+            default: "solid-gray"
+        },
+        bgGradientDirection: {
+            type: String
+        },
+        bgImage: {
+            type: String
+        },
+        registerButton: {
+            type: Boolean,
+            default: true
+        }
     },
-    loginButtonDesign: {
-      type: String,
-      default: "filled"
-    },
-    loginButtonColor: {
-      type: String,
-      default: "green"
-    },
-    bgColor: {
-      type: String,
-      default: "solid-gray"
-    },
-    bgGradientDirection: {
-      type: String
-    },
-    bgImage: {
-      type: String
-    },
-    registerButton: {
-      type: Boolean,
-      default: true
-    }
-  },
-  setup() {
-    const { deviceType } = windowSizeCalculator();
+    setup() {
+        /*Device type*/
+        const {deviceType} = windowSizeCalculator();
 
-    return { deviceType };
-  },
-  data() {
-    return {
-      form: this.$inertia.form({
-        email: "",
-        password: "",
-        remember: false
-      })
-    };
-  },
+        /* Dark Mode */
+        const {darkMode, appearingMode, changeTheme} = darkModeFn();
 
-  methods: {
-    submit() {
-      this.form
-        .transform(data => ({
-          ...data,
-          remember: this.form.remember ? "on" : ""
-        }))
-        .post(this.route("login"), {
-          onFinish: () => this.form.reset("password")
+        /* Multi-language */
+        const {changeLang, locale} = langChooserFn();
+
+        const {t, tm} = useI18n({
+            inheritLocale: true,
+            messages: authTranslates,
         });
-    },
-    beforeStyle(event) {
-      event.style.opacity = 0;
-    },
-    enterStyle(event) {
-      event.style.opacity = 1;
-      event.style.transition = `all 1s linear`;
-    }
-  },
-  computed: {
-    errors() {
-      return this.$page.props.errors;
+
+        /* Login */
+        const form = useForm({
+            email: "",
+            password: "",
+            remember: false,
+        });
+
+        /* Client-side Validation */
+        const rules = ref({
+            email: {required, email},
+            password: {required},
+        });
+        const v = useVuelidate(rules, form, {$lazy: true});
+
+        const submit = async () => {
+            if (!(await v.value.$validate())) {
+                return;
+            }
+            form
+                .transform((data) => ({
+                    ...data,
+                    remember: form.remember ? "on" : "",
+                }))
+                .post(route("login"), {
+                    onFinish: () => form.reset("password"),
+                });
+        };
+
+
+        const bgIndex = ref(0)
+        const changeBg = ()=>{
+            if(authBgImages[appearingMode.value].length-1>bgIndex.value){
+                bgIndex.value++
+            }else{
+                bgIndex.value = 0
+            }
+        };
+        const activeBg = computed(()=>{
+            return authBgImages[appearingMode.value][bgIndex.value].url
+        })
+
+        return {
+            form,
+            darkMode,
+            appearingMode,
+            changeLang,
+            changeTheme,
+            changeBg,
+            activeBg,
+            submit,
+            languages,
+            locale,
+            deviceType,
+            appConf,
+            authScreenConf,
+            t,
+            tm
+        };
     },
 
-    hasErrors() {
-      return Object.keys(this.errors).length > 0;
+    methods: {
+        beforeStyle(event) {
+            event.style.opacity = 0;
+        },
+        enterStyle(event) {
+            event.style.opacity = 1;
+            event.style.transition = `all 1s linear`;
+        }
+    },
+    computed: {
+        errors() {
+            return this.$page.props.errors;
+        },
+
+        hasErrors() {
+            return Object.keys(this.errors).length > 0;
+        }
     }
-  }
 });
 </script>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,98 +1,214 @@
 <template>
-    <Head title="Log in" />
-
-    <jet-authentication-card>
+    <Head :title="tm('login')"/>
+    <t-login
+        design="{
+        bgColor: '',
+        headerColor: '',
+        radius: 3,
+        bgImage: {
+        dark: '',
+        light: ''
+        },
+        loginButton: {
+        dark: '',
+        light: ''
+        },
+        registerButton: {
+        dark: '',
+        light: ''
+        },
+        }"
+        :bg-image="activeBg"
+        color="solid-purple"
+        :radius="authScreenConf.radius ? authScreenConf.radius : appConf.radius"
+    >
         <template #logo>
-            <jet-authentication-card-logo />
+            <div :class="authScreenConf.logoAreaClasses">
+                <img
+                    :src="[
+                        appearingMode === 'dark' ? authScreenConf.darkLogo ? authScreenConf.darkLogo : appConf.darkLogo :
+                        authScreenConf.lightLogo ? authScreenConf.lightLogo : appConf.lightLogo
+                    ]"
+                    :class="authScreenConf.logoClasses"
+                />
+                <span v-text="authScreenConf.appName ? authScreenConf.appName : appConf.appName" :class="authScreenConf.appNameClasses"></span>
+            </div>
         </template>
+    </t-login>
+    <div class="fixed bottom-0 flex z-50 w-full justify-center sm:justify-end space-x-6 p-6">
+        <!--Dark Mode-->
+        <div
+            class="flex justify-center items-center bg-slate-100/50 hover:bg-slate-800/50 dark:text-slate-100 dark:bg-slate-800/75 dark:hover:bg-slate-100/75 dark:hover:text-slate-700 hover:text-slate-100 p-4 w-10 h-10 cursor-pointer overflow-hidden"
+            :class="`radius-${authScreenConf.radius ? authScreenConf.radius : appConf.radius}`"
+            @click="changeTheme"
+        >
 
-        <jet-validation-errors class="mb-4" />
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+            <transition mode="out-in" name="darkModeTransition">
+                <!-- Light -->
+                <icon
+                    v-if="darkMode === 'light'"
+                    icon="sun"
+                    size="lg"
+                    key="light"
+                    :alt="tm('lightMode')"
+                    class="transform hover:scale-110 active:scale-90 transition-transform duration-300"
+                />
+                <!-- Auto -->
+                <icon
+                    v-else-if="darkMode === 'auto'"
+                    icon="palette"
+                    size="lg"
+                    key="auto"
+                    :alt="tm('auto')"
+                    class="transform hover:scale-110 active:scale-90 transition-transform duration-300"
+                />
+                <!-- Dark -->
+                <icon
+                    v-else
+                    icon="moon"
+                    size="lg"
+                    key="dark"
+                    :alt="tm('darkMode')"
+                    class="transform hover:scale-110 active:scale-90 transition-transform duration-300"
+                />
+            </transition>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <jet-checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
+        <!--Change Background-->
+        <div
+            @click="changeBg"
+            class="flex justify-center items-center group bg-slate-100/50 hover:bg-slate-800/50 dark:text-slate-100 dark:bg-slate-800/75 dark:hover:bg-slate-100/75 dark:hover:text-slate-700 hover:text-slate-100 p-4 w-10 h-10 cursor-pointer"
+            :class="`radius-${authScreenConf.radius ? authScreenConf.radius : appConf.radius}`"
+        >
+            <icon icon="redo" class="transform group-hover:scale-110 group-active:scale-90 transition-transform duration-300"/>
+        </div>
+    </div>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetCheckbox from '@/Jetstream/Checkbox.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
 
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetCheckbox,
-            JetLabel,
-            JetValidationErrors,
-            Link,
-        },
+/*Main functions*/
+import {computed, defineComponent, ref} from "vue";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+import darkModeFn from "@/Functions/darkMode";
+import useVuelidate from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 
-        props: {
-            canResetPassword: Boolean,
-            status: String
-        },
+/*Sources*/
+import {appConf, authScreenConf} from "@/config";
+import {authBgImages} from "@/Sources/bgImages";
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    email: '',
-                    password: '',
-                    remember: false
-                })
+/* Multi language */
+import {useI18n} from "vue-i18n";
+import langChooserFn from "@/Functions/langChooser"
+import {languages, flags, authTranslates} from "@/Lang/languages";
+
+/*Fontawesome icons*/
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faSun, faMoon, faPalette, faRedo} from "@fortawesome/free-solid-svg-icons";
+import TLogin from "@/Components/Auth/TLogin";
+library.add(faSun, faMoon, faPalette, faRedo)
+
+
+export default defineComponent({
+    components: {
+        TLogin,
+        Head,
+        ...flags,
+    },
+    props: {
+        canResetPassword: Boolean,
+        status: String,
+    },
+    setup() {
+        /* Dark Mode */
+        const {darkMode, appearingMode, changeTheme} = darkModeFn();
+
+        /* Multi-language */
+        const {changeLang, locale, loadingTranslations} = langChooserFn();
+
+        const {t, tm} = useI18n({
+            inheritLocale: true,
+            messages: authTranslates,
+        });
+
+        /* Login */
+        const form = useForm({
+            email: "",
+            password: "",
+            remember: false,
+        });
+
+        /* Client-side Validation */
+        const rules = ref({
+            email: {required, email},
+            password: {required},
+        });
+        const v = useVuelidate(rules, form, {$lazy: true});
+
+        const submit = async () => {
+            if (!(await v.value.$validate())) {
+                return;
             }
-        },
+            form
+                .transform((data) => ({
+                    ...data,
+                    remember: form.remember ? "on" : "",
+                }))
+                .post(route("login"), {
+                    onFinish: () => form.reset("password"),
+                });
+        };
 
-        methods: {
-            submit() {
-                this.form
-                    .transform(data => ({
-                        ... data,
-                        remember: this.form.remember ? 'on' : ''
-                    }))
-                    .post(this.route('login'), {
-                        onFinish: () => this.form.reset('password'),
-                    })
+
+        const bgIndex = ref(0)
+        const changeBg = ()=>{
+            if(authBgImages[appearingMode.value].length-1>bgIndex.value){
+                bgIndex.value++
+            }else{
+                bgIndex.value = 0
             }
-        }
-    })
+        };
+        const activeBg = computed(()=>{
+            return authBgImages[appearingMode.value][bgIndex.value].url
+        })
+
+
+        return {
+            appConf,
+            authScreenConf,
+            bgIndex,
+            darkMode,
+            authBgImages,
+            appearingMode,
+            changeBg,
+            changeTheme,
+            form,
+            languages,
+            locale,
+            loadingTranslations,
+            changeLang,
+            activeBg,
+            v,
+            t,
+            tm,
+            submit,
+        };
+    }
+})
 </script>
+<style scoped>
+.darkModeTransition-enter-active,
+.darkModeTransition-leave-active {
+    transition: all 500ms ease-in-out;
+}
+
+.darkModeTransition-enter-from,.darkModeTransition-leave-to {
+    opacity: 0;
+    transform: scale(0) rotateZ(-360deg);
+}
+
+.darkModeTransition-enter-to,.darkModeTransition-leave-from  {
+    opacity: 1;
+    transform: scale(1);
+}
+</style>
