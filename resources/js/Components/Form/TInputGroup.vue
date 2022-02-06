483bd43dbd974407f80c2f423a380b5f7f50ae73
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-02-06 22:29:09 +0300

    adds width option to input group component - phoneWidth (default: 12) - tabletWidth (default: 6) - desktopWidth (default: 6)

--- resources/js/Components/Form/TInputGroup.vue
+++ resources/js/Components/Form/TInputGroup.vue
@@ -1,50 +1,51 @@
 <template>
-    <div class="flex flex-col">
+    <div
+        class="flex flex-col"
+        :class="[
+            `grid-${phoneWidth}`,
+            `grid-md-${tabletWidth}`,
+            `grid-lg-${desktopWidth}`
+        ]"
+    >
         <!--Label-->
         <label
             :for="labelFor"
             class="flex flex-row gap-1 items-center font-medium text-sm whitespace-normal dark:text-gray-200"
         >
             <!--Label-->
-            <span v-if="label">
-        {{ label }}
-      </span>
+            <span v-if="label">{{ label }}</span>
             <!--Label Prop-->
             <span v-if="!label && $slots.label" class="inline-flex items-center">
-        <!--Label Slot-->
-        <slot name="label"/>
-      </span>
+                <!--Label Slot-->
+                <slot name="label" />
+            </span>
             <!--Sub Label-->
             <span
                 v-if="subLabel"
-                :class="['text-xs italic',subLabelColors[subLabelColor]]"
-            >
-        {{ subLabel }}
-      </span>
+                :class="['text-xs italic', subLabelColors[subLabelColor]]"
+            >{{ subLabel }}</span>
         </label>
         <!--Input Content-->
         <slot></slot>
         <!--Front End Error-->
-        <div v-if="errors.length>0">
+        <div v-if="errors.length > 0">
             <template v-for="error in errors" :key="error.$uid">
-                <p class="text-sm text-red-600 mt-2 whitespace-nowrap">
-                    {{ error.$message }}
-                </p>
+                <p class="text-sm text-red-600 mt-2 whitespace-nowrap">{{ error.$message }}</p>
             </template>
         </div>
 
         <!--Backend Error-->
         <div v-else>
-                <p class="text-sm text-red-600 mt-2 whitespace-nowrap">
-                    {{ $page.props.errors[labelFor] }}
-                </p>
+            <p
+                class="text-sm text-red-600 mt-2 whitespace-nowrap"
+            >{{ $page.props.errors[labelFor] }}</p>
         </div>
     </div>
 </template>
 
 <script>
-import {defineComponent, provide, toRefs} from 'vue'
-import {inputGroupStyleMixin} from "@/Mixins/Styles/inputGroupStyleMixin";
+import { defineComponent, provide, toRefs } from 'vue'
+import { inputGroupStyleMixin } from "@/Mixins/Styles/inputGroupStyleMixin";
 
 export default defineComponent({
     props: {
@@ -69,12 +70,24 @@ export default defineComponent({
             default() {
                 return []
             }
+        },
+        phoneWidth: {
+            type: Number,
+            default: 12
+        },
+        tabletWidth: {
+            type: Number,
+            default: 6
+        },
+        desktopWidth: {
+            type: Number,
+            default: 6
         }
     },
     mixins: [inputGroupStyleMixin],
     setup(props) {
         /*Error Status Provide to Input Component*/
-        const {errors} = toRefs(props);
+        const { errors } = toRefs(props);
         provide('errors', errors);
     }
 })
