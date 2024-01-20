c28e0f6eee4e273fdce705bd367dedefd08f97b9
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-01-20 23:22:37 +0300

    add pinia state management lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,40 +1,47 @@
-import './bootstrap';
-import '../css/app.css';
-import '../sass/app.sass';
+import "./bootstrap";
+import "../css/app.css";
+import "../sass/app.sass";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
 /* Multi-language */
 import { createI18n } from "vue-i18n";
-import {languages} from "./Lang";
+import { languages } from "./Lang";
+
 const i18n = createI18n({
     legacy: false,
     locale: "en",
     fallbackLocale: "en",
     fallbackRoot: "en",
-    messages: languages.map(lang => ({[lang.id]: lang.translates})).reduce((a, b) => ({...a, ...b}), {}),
+    messages: languages.map(lang => ({ [lang.id]: lang.translates })).reduce((a, b) => ({ ...a, ...b }), {})
 });
 
 /* Highlighter */
-import VueHighlightJS from 'vue3-highlightjs'
+import VueHighlightJS from "vue3-highlightjs";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+
+/*Pinia*/
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob("./Pages/**/*.vue")),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(i18n)
             .use(VueHighlightJS)
+            .use(pinia)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
-    },
+        color: "#4B5563"
+    }
 });
