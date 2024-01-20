857a16b24be1b76bc7b23e607006fea700c171f8
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-01-20 13:44:31 +0300

    add laravel-lang lib for backed multi-lang
    -https://laravel-lang.com/installation.html

--- config/app.php
+++ config/app.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Translation\TranslationServiceProvider as BaseTranslationServiceProvider;
+use LaravelLang\JsonFallback\TranslationServiceProvider as JsonTranslationServiceProvider;
 
 return [
 
@@ -170,6 +172,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+    ])->replace([
+        BaseTranslationServiceProvider::class => JsonTranslationServiceProvider::class,
     ])->toArray(),
 
     /*
