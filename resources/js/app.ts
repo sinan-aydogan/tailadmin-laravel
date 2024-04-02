b8b3162a56cf9dc412cb853c01f687e16ba0ed55
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-04-02 22:20:54 +0300

    feat: adds iconify and tabler support
    - iconify: (documentation)[https://iconify.design/docs/icon-components/vue/]
    - tabler icons(default): (documentation)[https://tabler.io/docs/icons/vue]
    
    You can install other icon sets.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { createPinia } from 'pinia'
+import { Icon } from '@iconify/vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +20,7 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(pinia)
+            .component('icon', Icon)
             .use(ZiggyVue)
             .mount(el);
     },
