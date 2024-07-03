38f80e890b8c0310f06cdff48ab6ef9945fc59fb
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-07-04 01:07:53 +0300

    refactor: main files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import scrollbar from "tailwind-scrollbar"
 
-module.exports = {
-    darkMode: 'class',
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './resources/js/**/*.js',
+        './resources/js/**/*.{js,ts,json,vue}',
     ],
 
     theme: {
@@ -39,5 +41,5 @@ module.exports = {
         scrollbar: ['dark']
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('tailwind-scrollbar')],
+    plugins: [forms, typography, scrollbar],
 };
