d1bae5a406f5d0f5d4a791ee42c664d72c7f2850
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-07-03 23:45:35 +0300

    refactor: factory

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -2,25 +2,20 @@
 
 namespace Database\Factories;
 
-use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
 class TeamFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Team::class;
-
-    /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->unique()->company(),
