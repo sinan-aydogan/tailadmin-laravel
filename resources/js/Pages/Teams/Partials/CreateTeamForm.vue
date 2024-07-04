d7eef2ebae2510141b93a4b8839958495037578d
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-07-04 22:53:28 +0300

    refactor: replace old auth pages with new jetstream pages

--- resources/js/Pages/Teams/Partials/CreateTeamForm.vue
+++ resources/js/Pages/Teams/Partials/CreateTeamForm.vue
@@ -1,5 +1,25 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+import FormSection from '@/Jetstream/FormSection.vue';
+import InputError from '@/Jetstream/InputError.vue';
+import InputLabel from '@/Jetstream/InputLabel.vue';
+import PrimaryButton from '@/Jetstream/PrimaryButton.vue';
+import TextInput from '@/Jetstream/TextInput.vue';
+
+const form = useForm({
+    name: '',
+});
+
+const createTeam = () => {
+    form.post(route('teams.store'), {
+        errorBag: 'createTeam',
+        preserveScroll: true,
+    });
+};
+</script>
+
 <template>
-    <jet-form-section @submitted="createTeam">
+    <FormSection @submitted="createTeam">
         <template #title>
             Team Details
         </template>
@@ -10,65 +30,37 @@
 
         <template #form>
             <div class="col-span-6">
-                <jet-label value="Team Owner" />
+                <InputLabel value="Team Owner" />
 
                 <div class="flex items-center mt-2">
-                    <img class="object-cover w-12 h-12 rounded-full" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name">
+                    <img class="object-cover w-12 h-12 rounded-full" :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
 
-                    <div class="ml-4 leading-tight">
-                        <div>{{ $page.props.user.name }}</div>
-                        <div class="text-sm text-gray-700">{{ $page.props.user.email }}</div>
+                    <div class="ms-4 leading-tight">
+                        <div class="text-gray-900 dark:text-white">{{ $page.props.auth.user.name }}</div>
+                        <div class="text-sm text-gray-700 dark:text-gray-300">
+                            {{ $page.props.auth.user.email }}
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-span-6 sm:col-span-4">
-                <jet-label for="name" value="Team Name" />
-                <jet-input id="name" type="text" class="block w-full mt-1" v-model="form.name" autofocus />
-                <jet-input-error :message="form.errors.name" class="mt-2" />
+                <InputLabel for="name" value="Team Name" />
+                <TextInput
+                    id="name"
+                    v-model="form.name"
+                    type="text"
+                    class="block w-full mt-1"
+                    autofocus
+                />
+                <InputError :message="form.errors.name" class="mt-2" />
             </div>
         </template>
 
         <template #actions>
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                 Create
-            </jet-button>
+            </PrimaryButton>
         </template>
-    </jet-form-section>
+    </FormSection>
 </template>
-
-<script>
-    import { defineComponent } from 'vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetFormSection from '@/Jetstream/FormSection.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import JetInputError from '@/Jetstream/InputError.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-
-    export default defineComponent({
-        components: {
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-        },
-
-        data() {
-            return {
-                form: this.$inertia.form({
-                    name: '',
-                })
-            }
-        },
-
-        methods: {
-            createTeam() {
-                this.form.post(route('teams.store'), {
-                    errorBag: 'createTeam',
-                    preserveScroll: true
-                });
-            },
-        },
-    })
-</script>
