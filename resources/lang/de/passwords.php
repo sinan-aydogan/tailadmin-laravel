486f34a7b58b6b835ebc22bf672f89f7c0e155ff
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-07-09 05:58:11 +0300

    adds: laravel-lang support
    Documentation: https://laravel-lang.com/configuration.html#inline

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Password Reset Language Lines
-|--------------------------------------------------------------------------
-|
-| The following language lines are the default lines which match reasons
-| that are given by the password broker for a password update attempt
-| has failed, such as for an invalid token or invalid new password.
-|
-*/
+declare(strict_types=1);
 
 return [
     'reset'     => 'Das Passwort wurde zurückgesetzt!',
-    'sent'      => 'Passworterinnerung wurde gesendet!',
+    'sent'      => 'E-Mail zum Zurücksetzen des Passworts wurde gesendet!',
     'throttled' => 'Bitte warten Sie, bevor Sie es erneut versuchen.',
-    'token'     => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig oder abgelaufen.',
+    'token'     => 'Der Passwort-Wiederherstellungsschlüssel ist ungültig oder abgelaufen.',
     'user'      => 'Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.',
 ];
