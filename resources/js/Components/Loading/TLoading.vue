cd5f0ffc96f456fdee5962cd6e5a2b46af1015bf
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2024-07-11 00:09:35 +0300

    fix: close button styles

--- resources/js/Components/Loading/TLoading.vue
+++ resources/js/Components/Loading/TLoading.vue
@@ -101,20 +101,9 @@ const hasSlot = name => !!useSlots()[name];
             </div>
             <!--Close Icon-->
             <div v-if="closeable" class="loading-screen-close" @click="close">
-                <svg
-                    class="h-10 w-10"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M6 18L18 6M6 6l12 12"
-                    />
-                </svg>
+
+                <iconify-icon icon="mingcute:close-circle-line" class="h-10 w-10"/>
+
             </div>
             <!--Content-->
             <div v-if="!hasSlot('default')" class="loading-screen-content">
